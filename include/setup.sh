e283c4020e39c99070d140a98de260f68fa4502f
Jaclyn Bibby <jaclyn.bibby@liv.ac.uk>
2012-04-05 16:04:31 +0100

    removed LGA referecnes and cleaned up exits

--- include/setup.sh
+++ include/setup.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=$PYTHONPATH:/home/jac45/ample/python
-export ROSETTA_PATH=/home/jac45/rosetta
+export ROSETTA_PATH=/home/jaclyn/programs/rosetta3.1_Bundles
+export PYTHONPATH=$PYTHONPATH:/home/jaclyn/DOMAINS/ample/python
 
