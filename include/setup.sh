9f593d44b8aef71c9fe987658cac3e7ebe2b4ad4
Jaclyn Bibby <jaclyn.bibby@liv.ac.uk>
2012-04-05 15:56:25 +0100

    removed environment paths from anple and setup

--- include/setup.sh
+++ include/setup.sh
@@ -1,6 +1,3 @@
-export THESEUS_EXE=/share/apps/bin/theseus
-export SCWRL_EXE=/share/apps/bin/Scwrl4
 export PYTHONPATH=$PYTHONPATH:/home/jac45/ample/python
 export ROSETTA_PATH=/home/jac45/rosetta
-export MAX_EXE=/home/jac45/maxcluster
-export SPICKER_EXE=/home/jac45/spicker_dir/spicker
+
