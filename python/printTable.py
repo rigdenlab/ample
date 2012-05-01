a1080ac40f8174e4332769531d8dd0226ac2d9e1
Jaclyn Bibby <jaclyn.bibby@liv.ac.uk>
2012-05-01 11:04:39 +0100

    printTable

--- python/printTable.py
+++ python/printTable.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 import locale
+import os, sys, re
 locale.setlocale(locale.LC_NUMERIC, "")
 
+
 class Table:
+
+   def __init__(self):   
+      self.bumppath = ''
+      self.cluster = ''
 
    def format_num(self, num):
        """Format a number according to given places.
@@ -45,12 +51,144 @@ class Table:
                print >> out, col,
            print >> out
 
+
+
+
+
+
+
+
+   def maketable(self):
+       table = []
+       
+
+       if self.cluster == False:
+           for run in os.listdir(self.bumppath):
+               if re.search('search', run): 
+                   print run 
+                   name = re.split('_', run)
+                   sidechains = name[1]
+                   if sidechains == 'SCWRL':
+                       trunc = name[5]
+                       rad = name[7]
+                   else:
+                       trunc = name[4]
+                       rad = name[6] 
+                   trunc = str(round(float(trunc)))
+                   #phaser
+                   phasersucc = ''
+                   phaserCC = 'none'
+                   phaserFreeR = 'none'
+                   if os.path.exists(os.path.join(self.bumppath, run, 'phaser_shelx' ) ):
+                         if os.path.exists(    os.path.join(self.bumppath, run, 'phaser_shelx', 'orig.pdb' )):
+                                for line in  open(os.path.join(self.bumppath, run, 'phaser_shelx', 'orig.pdb' )):
+                                           if re.search('CC', line):     
+                                                 split = re.split('CC\s*=\s*(\d*\.\d*)',line)
+                                                 score = split[1]
+                                                 phaserCC = score  
+
+                         if os.path.exists(    os.path.join(self.bumppath, run, 'phaser_shelx', 'refined.pdb' )):  
+                                for line in  open(os.path.join(self.bumppath, run, 'phaser_shelx', 'refined.pdb' )):
+                                           if re.search('REMARK\s*(\d*)\s*FREE R VALUE\s*\:', line):     
+                                                 split = re.split('REMARK\s*(\d*)\s*FREE R VALUE\s*\:\s*(\d*\.\d*)',line)
+                                                 score = split[2]
+                                                 phaserFreeR = score  
+
+                      
+                   print phaserCC 
+                   if phaserCC != 'none': 
+                      if float(phaserCC) > 25:
+                         phasersucc = 'Solution'
+                      else:
+                         phasersucc = ''
+                      table.append([phasersucc,'',trunc, sidechains, 'Phaser',phaserFreeR,float(phaserCC)])
+
+
+
+
+                   #molrep
+                   molrepsucc = ''
+                   molrepCC = 'none'
+                   molrepFreeR = 'none'
+                   if os.path.exists(os.path.join(self.bumppath, run, 'molrep_shelx' ) ):
+                         if os.path.exists(    os.path.join(self.bumppath, run, 'molrep_shelx', 'orig.pdb' )):
+                                for line in  open(os.path.join(self.bumppath, run, 'molrep_shelx', 'orig.pdb' )):
+                                           if re.search('CC', line):     
+                                                 split = re.split('CC\s*=\s*(\d*\.\d*)',line)
+                                                 score = split[1]
+                                                 molrepCC = score  
+
+                         if os.path.exists(    os.path.join(self.bumppath, run, 'molrep_shelx', 'refined.pdb' )):  
+                                for line in  open(os.path.join(self.bumppath, run, 'molrep_shelx', 'refined.pdb' )):
+                                           if re.search('REMARK\s*(\d*)\s*FREE R VALUE\s*\:', line):     
+                                                 split = re.split('REMARK\s*(\d*)\s*FREE R VALUE\s*\:\s*(\d*\.\d*)',line)
+                                                 score = split[2]
+                                                 molrepFreeR = score   
+                      
+                   print molrepCC 
+                   if molrepCC  != 'none':
+                      if float(molrepCC) > 25:
+                         molrepsucc = 'Solution'
+                      else:
+                         molrepsucc = ''
+                      table.append([molrepsucc,'',trunc, sidechains, 'Molrep',molrepFreeR,float(molrepCC)])
+
+
+
+       if self.cluster == True:
+
+           for run in os.listdir(self.bumppath):
+             if os.path.exists():
+               if re.search('search', run): 
+                   print run 
+                   name = re.split('_', run)
+                   sidechains = name[1]
+                   if sidechains == 'SCWRL':
+                       trunc = name[5]
+                       rad = name[7]
+                   else:
+                       trunc = name[4]
+                       rad = name[6] 
+                   trunc = str(round(float(trunc)))
+
+
+
+
+
+
+       table.sort(key=lambda x: x[6])
+       table = table[::-1]
+       x=1
+       for i in table:
+           print i
+           i[1] = x
+           x+=1
+       table.insert(0,["", "Ensemble No", "Truncation level", "Side chain type", "MR program", "Final Rfree", "Shelxe CC"] )
+       return table
+        
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
+    T=Table()
+    T.bumppath = '/home/jaclyn/Ample_tests/toxd-example/ROSETTA_MR_1/MRBUMP_cluster1'
+    T.cluster = False
+    table = T.maketable()
+    out = sys.stdout
+    T.pprint_table(out, table)  
+    
+
     table = [["", "Ensemble No", "Truncation level", "Side chain type", "MR program", "Final Rfree", "Shelxe CC"],
         ["spam", 1, 2, "All", "Phaser", 0.25, 23.51],
         ["eggs", 2, 6, "Reliable", "Molrep", 0.45, 4.56],
         ["lumberjacks", 3, 10, "Poly", "Phaser", 0.53, 12.12]]
     import sys
     out = sys.stdout
-    T=Table()
+
     T.pprint_table(out, table)
