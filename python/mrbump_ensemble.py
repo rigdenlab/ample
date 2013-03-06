21e8bdfc3dbd134ab71a7d2f4c211b5353720bb3
Jens Thomas <linucks42@gmail.com>
2013-03-06 16:14:41 +0000

    Working version of new way to run MRBUMP

--- python/mrbump_ensemble.py
+++ python/mrbump_ensemble.py
@@ -9,6 +9,7 @@ import logging
 import multiprocessing
 import subprocess
 import os
+import Queue
 import re
 import shutil
 import sys
@@ -101,7 +102,7 @@ def mrbump_ensemble_local( ensembles, amoptd, clusterID="X" ):
     
     # Now start the jobs
     processes = []
-    for i in range( amoptd['nprocs'] ):
+    for i in range( amoptd['nproc'] ):
         process = multiprocessing.Process(target=worker, args=( queue, amoptd['early_terminate'] ) )
         process.start()
         processes.append(process)
@@ -127,9 +128,11 @@ def mrbump_ensemble_local( ensembles, amoptd, clusterID="X" ):
                 # Finished so see what happened
                 if process.exitcode == 0 and amoptd['early_terminate']:
                     # Got a successful completion                    
-                    print "EARLY TERMINATE"
+                    #print "EARLY TERMINATE"
                     killall=True
                 done+=1
+                
+    print results_summary( os.getcwd() )
     
     
 #    # At this point all jobs have finished
@@ -159,6 +162,37 @@ def mrbump_ensemble_local( ensembles, amoptd, clusterID="X" ):
         
 ##End mrbump_ensemble_local
 
+def create_jobscript( name, pdb, amoptd, directory=None ):
+    """
+    Create the script to run MrBump for this PDB.
+    
+    Args:
+    name -- used to identify job and name the run script
+    pdb -- the path to the pdb file for this job
+    amoptd -- dictionary with job options
+    directory -- directory to write script to - defaults to cwd
+    
+    Returns:
+    path to the script
+    """
+    
+    # Path
+    if not directory:
+        directory = os.getcwd()
+    script_path = directory + os.sep + name + '.sub'
+    
+    # Write script
+    job_script = open(script_path, "w")
+    job_script.write('#!/bin/sh\n')
+    jobstr = mrbump_cmd.mrbump_cmd( amoptd, jobid=name, ensemble_pdb=pdb )
+    job_script.write(jobstr)
+    job_script.close()
+    
+    # Make executable
+    os.chmod(script_path, 0o777)
+    
+    return script_path
+##End create_jobscript
 
 def  worker( queue, early_terminate=False ):
     """
@@ -177,10 +211,11 @@ def  worker( queue, early_terminate=False ):
     
     while True:
         try:
+            # We use false to generate an exception when the Queue is empty
             mrb_script = queue.get(False)
-            print "worker {} got script {}".format(os.getpid(), mrb_script )
+            #print "worker {} got script {}".format(os.getpid(), mrb_script )
         except Queue.Empty:
-            print "worker {} got empty queue {}".format(os.getpid(),e)
+            #print "worker {} got empty queue {}".format(os.getpid(),e)
             break
         
         # Got a script so run
@@ -192,7 +227,7 @@ def  worker( queue, early_terminate=False ):
         
         # Can we use the retcode to check?
         # REM - is retcode object
-        print "worker {} GOT retcode!".format(os.getpid(), retcode )
+        #print "worker {} GOT retcode!".format(os.getpid(), retcode )
         
         # Run directory is name of script with search_ prepended and _mrbump appended
         directory = "search_" + name + "_mrbump"
@@ -200,7 +235,7 @@ def  worker( queue, early_terminate=False ):
         # Now check the result if early terminate
         if early_terminate:
             if check_success( directory ):
-                print "worker {} GOT SUCCESS!".format(os.getpid() )
+                #print "worker {} GOT SUCCESS!".format(os.getpid() )
                 return 0
         
     #print "worker {} FAILED!".format(os.getpid() )  
@@ -243,95 +278,44 @@ def check_success( directory ):
     # Nothing good enough
     return False
 
-    
-
-def create_jobscript( name, pdb, amoptd, directory=None ):
-    """
-    Create the script to run MrBump for this PDB.
-    
-    Args:
-    name -- used to identify job and name the run script
-    pdb -- the path to the pdb file for this job
-    amoptd -- dictionary with job options
-    directory -- directory to write script to - defaults to cwd
-    
-    Returns:
-    path to the script
-    """
-    
-    # Path
-    if not directory:
-        directory = os.getcwd()
-    script_path = directory + os.sep + name + '.sub'
-    
-    # Write script
-    job_script = open(script_path, "w")
-    job_script.write('#!/bin/sh\n')
-    jobstr = mrbump_cmd.mrbump_cmd( amoptd, jobid=name, ensemble_pdb=pdb )
-    job_script.write(jobstr)
-    job_script.close()
-    
-    # Make executable
-    os.chmod(script_path, 0o777)
-    
-    return script_path
-    
-##End create_jobscript
-
 def results_summary( run_dir ):
     """
     Generate a summary of the MR BUMP results
     
     Args:
+    run_dir -- the directory the jobs were run from
     
     Returns:
     results results_file as a string
     """
     
-
-    r = ""    
-    r += '###########################################################################################\n'
-    r += '###########################################################################################\n'
-    r += '##                                                                                       ##\n'
-    r += '##                                                                                       ##\n'
-    r += 'Results for this Run: '
-
-    header = ''
-    for line in open(results_file):
-        if not re.search('PHASER', line)  and not re.search('MOLREP', line):
-            r += line
-            header = line
-        else:
-            r += line
+    # Criteria for success with shelx
+    SHELXSUCCESS = 25.0
 
     # Set up the results results_file headers
     resultsTable = []
-    HEADER=True
+    
+    hline = None
 
-    results_file = []
-    r += '\n\n  Overall Summary:\n\n'
-    #print header
+    # Check all results files and add their contents to the resultsTable
     for search in os.listdir(run_dir):
         if os.path.isdir(run_dir+os.sep+search):
-            res = run_dir+os.sep+search+'/results/resultsTable.dat'
+            res = run_dir + os.sep + search + '/results/resultsTable.dat'
             if os.path.exists(res):
+                firstline=True
                 for line in open(res):
-                    if  'MR_PROGRAM' in line.upper() and 'SOLUTION' in line.upper() and HEADER:
-                        HEADER=False
+                    if firstline:
+                        if not hline:
+                            hline = line.strip().split()
+                        firstline=False
+                        continue
                     if  re.search('PHASER', line)  or re.search('MOLREP', line):
                         resultsTable.append(line.split())
-                        #print line
-                        split =re.split('\s*', line)
-
-                        results_file.append(split)
-
-    header  =header.split()
-
-    Best = 'none'
 
     # sort results
+    use_shelx=True # For time being assume we always use shelx
     if use_shelx:
-        y = header.index('SHELXE_CC')
+        y = hline.index('SHELXE_CC')
     # print resultsTable
     # print y
         resultsTable.sort(key=lambda x: float(x[y]))
@@ -341,23 +325,20 @@ def results_summary( run_dir ):
         if float( resultsTable[0][y]) >=25:
             DIE = True
 
-    # print resultsTable
-    if not use_shelx:
-        y = header.index('final_Rfree')
+    else:
+        y = hline.index('final_Rfree')
 
         resultsTable.sort(key=lambda x: float(x[y]))
         best = resultsTable[0][0]
         prog = resultsTable[0][1]
 
+
+    # Rebuild the path that generated the result
     n= re.sub('loc0_ALL','search', best)
     n = re.sub('UNMOD','mrbump', n)
     Best = run_dir + os.sep + n + '/data/'+re.sub('_UNMOD','', best)+'/unmod/mr/'+prog.lower()+'/refine'
 
-
-    resultsTable.insert(0, header)
-    # for x in resultsTable:
-    #   print x
-
+    resultsTable.insert(0, hline)
 
     # Currently a hack - need a list with a write method
     class WriteList(list):
@@ -366,35 +347,44 @@ def results_summary( run_dir ):
 
     # Output the results results_file
     T=printTable.Table()
-
     #out = sys.stdout
     out = WriteList()
     T.pprint_table(out, resultsTable)
     
+    
+    header = """###########################################################################################
+###########################################################################################
+##                                                                                       ##
+##                                                                                       ##
+
+Overall Summary
+
+"""
+    
+    r = header
     # now add list to string
-    for l in out:
-        r += l
+    r += "".join(out)
     
-    
-    r += "\n\n\n"
-    r += 'Best results so far are in : '
+    r += '\nBest results so far are in :\n'
     r +=  Best
 
+    footer = """
 
-
-    r += '##                                                                                       ##\n'
-    r += '##                                                                                       ##\n'
-    r += '###########################################################################################\n'
-    r += '###########################################################################################\n'
+##                                                                                       ##
+##                                                                                       ##
+###########################################################################################
+###########################################################################################
+"""
     
+    r+= footer
+        
     return r
-
 
 
 class Test(unittest.TestCase):
 
 
-    def testLocal(self):
+    def XtestLocal(self):
         
         import glob
         
@@ -411,7 +401,7 @@ class Test(unittest.TestCase):
              'FREE' : 'FreeR_flag',
              'F' : 'FP',
              'SIGF' : 'SIGFP',
-             'nprocs' : 3,
+             'nproc' : 3,
              'domain_all_chains_fasta' : None,
              'domain_all_chains_pdb' : None,
              'ASU': None,
@@ -432,6 +422,21 @@ class Test(unittest.TestCase):
         dir = "/home/Shared/ample-dev1/examples/toxd-example/ROSETTA_MR_0/MRBUMP_cluster1/search_All_atom_trunc_21.848434_rad_2_mrbump"
         
         self.assertTrue( check_success( dir ) )
+        
+    def testResultsSummary(self):
+        
+        dir = "/opt/ample-dev1/python"
+        dir = "/opt/ample-dev1/examples/toxd-example/ROSETTA_MR_1/MRBUMP_cluster1"
+        
+        
+#        T = printTable.Table()
+#        T.bumppath = dir
+#        T.cluster = False
+#        table = T.maketable()
+#        out = sys.stdout
+#        T.pprint_table(out, table)
+
+        print results_summary( dir )
 
 
 if __name__ == "__main__":
