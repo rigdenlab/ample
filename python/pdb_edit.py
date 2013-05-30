785d71ae9482e6697e9e4a93d3b56ee9854bac9b
Jens Thomas <linucks42@gmail.com>
2013-05-30 15:38:06 +0100

    Start at PDB editor

--- python/pdb_edit.py
+++ python/pdb_edit.py
@@ -5,6 +5,7 @@ Created on 15 Apr 2013
 '''
 
 import re
+import types
 
 class PDBEdit(object):
     """Class for editing PDBs
@@ -99,4 +100,142 @@ class PDBEdit(object):
         
         #End for
         pdb_out.close()
-        pdb_in.close()          
+        pdb_in.close
+
+
+f = open("/Users/jmht/Documents/AMPLE/ample-dev1/python/3PP5.pdb")
+
+class PdbAtom(object):
+    """
+    COLUMNS        DATA  TYPE    FIELD        DEFINITION
+-------------------------------------------------------------------------------------
+ 1 -  6        Record name   "ATOM  "
+ 7 - 11        Integer       serial       Atom  serial number.
+13 - 16        Atom          name         Atom name.
+17             Character     altLoc       Alternate location indicator.
+18 - 20        Residue name  resName      Residue name.
+22             Character     chainID      Chain identifier.
+23 - 26        Integer       resSeq       Residue sequence number.
+27             AChar         iCode        Code for insertion of residues.
+31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
+39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
+47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
+55 - 60        Real(6.2)     occupancy    Occupancy.
+61 - 66        Real(6.2)     tempFactor   Temperature  factor.
+77 - 78        LString(2)    element      Element symbol, right-justified.
+79 - 80        LString(2)    charge       Charge  on the atom.
+"""
+    def __init__(self):
+        """Set up attributes"""
+        
+        self.LINEID = "ATOM"
+        self.serial = None
+        self.name = None
+        self.altLoc = None
+        self.resName = None
+        self.chainID = None
+        self.resSeq = None
+        self.iCode = None
+        self.x = None
+        self.y = None
+        self.z = None
+        self.occupancy = None
+        self.tempFactor = None
+        self.element = None
+        self.charge = None
+        
+    def fromLine(self,line):
+        """Initialise from the line from a PDB"""
+        
+        assert len(line) >= 54,"Line length was: {0}\n{1}".format(len(line),line)
+        
+        self.serial = int(line[6:11])
+        self.name = line[12:15]
+        # Use for all so None means an empty field
+        if line[16].strip():
+            self.altLoc = line[16]
+        self.resName = line[17:20]
+        if line[21].strip():
+            self.chainID = line[21]
+        self.resSeq = line[22:26]
+        if line[16].strip():
+            self.iCode = line[26]
+        self.x = float(line[30:38])
+        self.y = float(line[38:46])
+        self.z = float(line[46:54])
+        if len(line) >= 60 and line[54:60].strip():
+            self.occupancy = float(line[54:60])
+        if len(line) >= 65 and line[60:65].strip():
+            self.tempFactor = float(line[60:65])
+        if len(line) >= 78 and line[76:78].strip():
+            self.element = line[76:78]
+        if len(line) >= 80 and line[78:80].strip():
+            self.charge = line[78:80]
+    
+    def toLine(self):
+        """Create a line suitable for printing to a PDB file"""
+        
+        s = "ATOM  "
+        s += "{0:5d}".format( self.serial )
+        s += "{0:>4}".format( self.name ) # right aligned
+        if not self.altLoc:
+            s += " "
+        else:
+            s += "{0:1}".format( self.altLoc )
+        s += "{0:3}".format( self.resName )
+        if not self.chainID:
+            s += " "
+        else:
+            s += "{0:1}".format( self.chainID )
+        s += "{0:4}".format( self.resSeq )
+        if not self.iCode:
+            s += " "
+        else:
+            s += "{0:1}".format( self.iCode )
+        s += "{0: 8.3F}".format( self.x )
+        s += "{0: 8.3F}".format( self.y )
+        s += "{0: 8.3F}".format( self.z )
+        if not self.occupancy:
+            s += "      "
+        else:
+            s += "{0: 6.2F}".format( self.occupancy )
+        if not self.tempFactor:
+            s += "      "
+        else:
+            s += "{0: 6.2F}".format( self.tempFactor )
+        if not self.element:
+            s += "  "
+        else:
+            s += "{0:>2}".format( self.element )
+        if not self.charge:
+            s += "  "
+        else:
+            s += "{0:2d}".format( self.charge )
+            
+        return s
+        
+
+    def __str__(self):
+        """List the data attributes of this object"""
+        me = {}
+        for slot in dir(self):
+            attr = getattr(self, slot)
+            if not slot.startswith("__") and not ( isinstance(attr, types.MethodType) or
+              isinstance(attr, types.FunctionType) ):
+                me[slot] = attr
+            
+        return "{0} : {1}".format(self.__repr__(),str(me))
+
+for line in f:
+    line = line.strip()
+    
+    if line.startswith("ATOM"):
+        #a = PdbAtom().fromLine(line)
+        print line  
+        a = PdbAtom()
+        a.fromLine(line)
+        print a.toLine()
+        break
+    
+    
+
