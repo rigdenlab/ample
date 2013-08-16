4638c1fd287b4cfde8ddb2671a52668d562ea514
Jens Thomas <linucks42@gmail.com>
2013-08-16 08:59:19 +0100

    Refactored somewhat and added a method to output pir files.

--- python/fasta_parser.py
+++ python/fasta_parser.py
@@ -10,8 +10,11 @@ class FastaParser(object):
     def __init__( self ):
         """Initialise the object"""
         
+        self.title = None # title line
         self.length = None # The length of the parse fasta
-        self.seqStr = None # The fasta sequence (just AA) as a single string 
+        self.seqStr = None # The fasta sequence (just AA) as a single string
+        
+        self.MAXWIDTH=80 # maximum width of any line 
         self.formattedFasta = None # The reformatted fasta
 
     def _parse_fasta( self, fasta ):
@@ -20,9 +23,7 @@ class FastaParser(object):
         fasta -- list of strings or open filehandle to read from the fasta file
         """
         
-        self.formattedFasta = []
         sequence = ""
-        header=None
         for line in fasta:
             
             # remove whitespace and line-endings
@@ -31,20 +32,21 @@ class FastaParser(object):
             
             # Deal with header
             if line.startswith( ">" ):
-                if header:
+                if self.title:
                     raise RuntimeError,"There appears to be more than one sequence in your fasta.\nPlease remove all but the first."
-    
-                header = line[0:80]+"\n"
-                self.formattedFasta.append( header )
+                
+                if len(line) > self.MAXWIDTH:
+                    self.title = line[0:self.MAXWIDTH]
+                else:
+                    self.title = line
                 continue
             
             sequence += line
         
         aa = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
-        
         self.seqStr = ""
         
-        # Check for unwanted characters
+        # Check for unwanted characters and get canonical string and length
         self.length=0
         for char in sequence:
             char = char.upper()
@@ -53,24 +55,43 @@ class FastaParser(object):
             self.seqStr +=char
             self.length +=1
         
-        # Now reformat to 80 chars
-        llen=80
-        for chunk in range( 0, len(self.seqStr), llen ):
-            self.formattedFasta.append( self.seqStr[ chunk:chunk+llen ]+"\n"  )
-        
-        # Add last newline
-        self.formattedFasta.append("\n")
-        
         return
     
-    def getSeqStr(self, fastaFile ):
-        """Return the fasta string from the given file"""
+    def fastaStr(self):
+        """Return a canonical MAXWIDTH fasta representation of the file as a line-separated string"""
+    
+        formattedFasta = [ self.title + "\n" ]
+        # Now reformat to MAXWIDTH chars
+        for chunk in range( 0, len(self.seqStr), self.MAXWIDTH ):
+            formattedFasta.append( self.seqStr[ chunk:chunk+self.MAXWIDTH ]+"\n"  )
+            
+        # Add last newline
+        formattedFasta.append("\n")
         
-        f = open( fastaFile, "r")
-        self._parse_fasta( f )
-        f.close()
+        return formattedFasta
+    
+    def pirStr(self):
+        """Return a canonical MAXWIDTH PIR representation of the file as a line-separated string"""
+    
+        pirStr = [ self.title + "\n\n" ] # Title plus the obligatory blank line
         
-        return self.seqStr
+        # Now reformat to MAXWIDTH chars
+        for chunk in range( 0, len(self.seqStr), self.MAXWIDTH ):
+            pirStr.append( self.seqStr[ chunk:chunk+self.MAXWIDTH ]+"\n"  )
+            
+        # Add last newline
+        pirStr.append("\n")
+        
+        return pirStr
+    
+#     def getSeqStr(self, fastaFile ):
+#         """Return the fasta string from the given file"""
+#         
+#         f = open( fastaFile, "r")
+#         self._parse_fasta( f )
+#         f.close()
+#         
+#         return self.seqStr
         
     def reformat_fasta( self, input_fasta, output_fasta):
         """
@@ -87,12 +108,30 @@ class FastaParser(object):
         f.close()
         
         fasout=open( output_fasta, "w")
-        for line in self.formattedFasta:
+        for line in self.fastaStr():
             fasout.write( line )
         fasout.close()
         
         return
 
+    def toPir(self, input_fasta, output_pir=None ):
+        """Take a fasta file and output the corresponding PIR file"""
+
+        f = open( input_fasta, "r")
+        self._parse_fasta( f )
+        f.close()
+        
+        if not output_pir:
+            dir, ffile = os.path.split( input_fasta )
+            fname = os.path.splitext(ffile)[0]
+            output_pir = os.path.join( dir, fname+".pir" )
+        
+        pirout=open( output_pir, "w")
+        for line in self.pirStr():
+            pirout.write( line )
+        pirout.close()
+        
+        return   
 
 class Test(unittest.TestCase):
 
@@ -116,7 +155,7 @@ GDGAAATSD
 
 """
 
-        self.assertEqual( outfasta, "".join(fp.formattedFasta) )
+        self.assertEqual( outfasta, "".join( fp.fastaStr() ) )
         self.assertEqual( fp.length, 249)
   
               
