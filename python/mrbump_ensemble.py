914edda615fa079337b59fdc1a04613052de322d
Jens Thomas <linucks42@gmail.com>
2013-10-10 17:24:05 +0100

    Removed code to run jobs as now lives in separate modules.

--- python/mrbump_ensemble.py
+++ python/mrbump_ensemble.py
@@ -7,16 +7,13 @@ Created on Feb 28, 2013
 # python imports
 import logging
 import multiprocessing
-import subprocess
 import os
-import sys
-import time
 import unittest
 
 # our imports
-import ample_util
 import clusterize
 import mrbump_cmd
+import workers
 
 
 def generate_jobscripts( ensemble_pdbs, amoptd ):
@@ -62,7 +59,7 @@ def generate_jobscripts( ensemble_pdbs, amoptd ):
             
     # Reset amoptd
     if amoptd['split_mr']:
-       amoptd['mrbump_programs'] = mrbump_programs
+        amoptd['mrbump_programs'] = mrbump_programs
             
     if not len( job_scripts ):
         msg = "No job scripts created!"
@@ -169,120 +166,15 @@ def mrbump_ensemble_cluster( job_scripts, amoptd ):
 ##End mrbump_ensemble_cluster
 
 def mrbump_ensemble_local( job_scripts, amoptd ):
-    """
-    Process the list of ensembles using MrBump on a local machine
-    
-    Args:
-    job_scripts -- list of scripts to run mrbump
-    amoptd -- dictionary with job options
-    """
-
-    logger = logging.getLogger()
-    logger.info("Running MR and model building on a local machine")
-
-    # Queue to hold the jobs we want to run
-    queue = multiprocessing.Queue()
-    
-    # Add jobs to the queue
-    #logger.info("Generating MRBUMP runscripts in: {0}".format( os.getcwd() ) )
-    for script in job_scripts:
-        queue.put(script)
-
-    # Now start the jobs
-    processes = []
-    for i in range( amoptd['nproc'] ):
-        process = multiprocessing.Process(target=worker, args=( queue, amoptd['early_terminate'] ) )
-        process.start()
-        processes.append(process)
-
-    
-    # Loop through the processes checking if any are done
-    timeout=10*60
-    timeout=1*60
-    while len(processes):
-        
-        for i, process in enumerate(processes):
-            
-            # Join process for timeout seconds and if we haven't finished by then
-            # move onto the next process
-            process.join(timeout)
-            
-            if not process.is_alive():
-                #print "CHECKING COMPLETED PROCESS {0} WITH EXITCODE {1}".format(process,process.exitcode)
-                # Remove from processes to check
-                del processes[i]
-                
-                # Finished so see what happened
-                if process.exitcode == 0 and amoptd['early_terminate']:
-                    if not queue.empty():
-                        logger.info( "Process {0} was successful so removing remaining jobs from queue".format(process.name) )
-                        # Remove all remaining processes from the queue. We do this rather than terminate the processes
-                        # as terminating leaves the MRBUMP processes running. This way we hang around until all our
-                        # running processes have finished
-                        while not queue.empty():
-                            job = queue.get()
-                            logger.debug( "Removed job [{0}] from queue".format(job) )
-                        
-    # need to wait here as sometimes it takes a while for the results files to get written
-    time.sleep(3)
+    """Run ensembling locally"""
+    js = workers.JobServer()
+    js.setJobs( job_scripts )
+    js.start( nproc=amoptd['nproc'],
+              early_terminate=amoptd['early_terminate'],
+              check_success=check_success )
     return
 
-def worker( queue, early_terminate=False ):
-    """
-    Worker process to run MrBump jobs until no more left.
-    
-    Args:
-    queue -- a python Queue object
-    early_terminate -- bool - terminate on first success or continue running
-    
-    Returns:
-    0 if molecular replacement worked
-    1 if nothing found
-    
-    We keep looping, removing jobs from the queue until there are no more left.
-    
-    REM: This needs to import the main module that it lives in so maybe this should
-    live in a separate module?
-    """
-    
-    while True:
-        if queue.empty():
-            #print "worker {0} got empty queue {1}".format(os.getpid(),e)
-            break
-
-        mrb_script = queue.get()
-        
-        # Got a script so run
-        # Get name from script
-        name = os.path.splitext( os.path.basename(mrb_script) )[0]
-        #logfile = name + ".log"
-        #f = open( logfile, "w")
-        print "Worker {0} running job {1}".format(multiprocessing.current_process().name, name)
-        
-        retcode = ample_util.run_command( [ mrb_script ], logfile=name + ".log", dolog=False)
-        #retcode = subprocess.call( [ mrb_script ], stdout=f, stderr=subprocess.STDOUT, cwd=None )
-        
-        # Can we use the retcode to check?
-        # REM - is retcode object
-        if retcode != 0:
-            print "WARNING! Worker {0} got retcode {1}".format(multiprocessing.current_process().name, retcode )
-        
-        # Run directory is name of script with search_ prepended and _mrbump appended
-        directory = os.path.join( os.getcwd(), "search_" + name + "_mrbump" )
-        
-        # Now check the result if early terminate
-        if early_terminate:
-            if check_success( directory ):
-                print "Worker {0} job succeeded".format(multiprocessing.current_process().name)
-                #return 0
-                sys.exit(0)
-        
-    #print "worker {0} FAILED!".format(multiprocessing.current_process().name)
-    #return 1
-    sys.exit(1)
-##End worker
-
-def check_success( directory ):
+def check_success( job ):
     """
     Check if a job ran successfully.
     
@@ -297,7 +189,9 @@ def check_success( directory ):
     
     SHELX_SUCCESS = 25.0
     
-    rfile = os.path.join(directory, 'results/resultsTable.dat')
+    directory, script = os.path.split( job )
+    scriptname = os.path.splitext( script )[0]
+    rfile = os.path.join(directory, 'search_'+scriptname+'_mrbump','results/resultsTable.dat')
     #print "{0} checking for file: {1}".format(multiprocessing.current_process().name,rfile)
     if not os.path.isfile(rfile):
         print "{0} cannot find results file: {1}".format(multiprocessing.current_process().name,rfile)
@@ -362,9 +256,9 @@ class Test(unittest.TestCase):
     
     def XtestSuccess(self):
         
-        tdir = "/home/jmht/t/ample-dev1/examples/toxd-example/ROSETTA_MR_3/MRBUMP/cluster_2/search_All_atom_trunc_0.551637_rad_3_mrbump"
+        pdbid = "/home/jmht/t/ample-dev1/examples/toxd-example/ROSETTA_MR_3/MRBUMP/cluster_2/search_All_atom_trunc_0.551637_rad_3_mrbump"
         
-        self.assertTrue( check_success( tdir ) )
+        self.assertTrue( check_success( pdbid ) )
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
