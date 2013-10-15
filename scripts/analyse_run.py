88baa12e4a8dea6ebcb4fa6e6048805618444810
Jens Thomas <linucks42@gmail.com>
2013-10-15 18:15:56 +0100

    CHange as dssp now in a separate module

--- scripts/analyse_run.py
+++ scripts/analyse_run.py
@@ -353,88 +353,7 @@ class CompareModels(object):
         return
 # End CompareModels
 
-class DsspParser(object):
-    """
-    Class 
-    """
 
-    def __init__(self,pfile):
-
-        self.dsspfile = pfile
-        
-        self.residues = []
-        self.assignment = []
-        
-        self.percentH = None
-        self.percentC = None
-        self.percentE = None
-
-        self.parse()
-
-        return
-    
-    def parse(self):
-        """parse"""
-
-        # print os.path.join(os.getcwd(), logfile)
-
-        self.residues = []
-        self.assignment = []
-        
-        capture=False
-        for line in open(self.dsspfile, 'r'):
-            
-            if "#  RESIDUE" in line:
-                capture=True
-                continue
-                
-            if capture:
-                #print "\"{0}\"".format(line)
-                #idx = int( line[0:5].strip() )
-                #resIdx = int( line[5:10].strip() )
-                #chainId = line[10:12].strip()
-                resName = line[12:14].strip()
-                assign = line[14:17].strip()
-                #print "\"{0}\"".format(line[14:17])
-                
-                # Only capture first chain
-                if "!" in assign:
-                    capture=False
-                    break
-                 
-                self.residues.append( resName )
-                self.assignment.append( assign )
-                
-        if not len(self.residues) or not len( self.assignment):
-            raise RuntimeError,"Got no assignment!"
-         
-        nH = 0
-        nC = 0
-        nE = 0
-        for p in self.assignment:
-            if p == "H":
-                nH += 1
-            elif p == "E":
-                nE += 1
-            # Just assume everything else is a coil
-            else:
-                nC += 1
-             
-        self.percentC = float(nC) / len(self.assignment) * 100
-        self.percentH = float(nH) / len(self.assignment) * 100
-        self.percentE = float(nE) / len(self.assignment) * 100
-            
-        return
-    
-    def asDict(self):
-        d = {}
-        d['assignment'] = self.assignment
-        d['residues'] = self.residues
-        d['percentC'] = self.percentC
-        d['percentE'] = self.percentE
-        d['percentH'] = self.percentH
-        
-        return d
 
 class EnsemblePdbParser(object):
     """
@@ -1455,7 +1374,7 @@ if __name__ == "__main__":
         sam_file = os.path.join( datadir, "fragments/t001_.rdb_ss2"  )
         psipredP = PsipredParser( sam_file )
         dssp_file = os.path.join( datadir, "{0}.dssp".format( pdbcode.lower()  )  )
-        dsspP = DsspParser( dssp_file )
+        dsspP = dssp.DsspParser( dssp_file )
         
         # Loop over each result
         #r = mrbump_results.ResultsSummary( os.path.join( datadir, "ROSETTA_MR_0/MRBUMP/cluster_1") )
