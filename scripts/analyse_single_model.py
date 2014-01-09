f64aff4b552afba14951b9b20354ad66990e0af9
Jens Thomas <linucks42@gmail.com>
2014-01-09 14:36:21 +0000

    run all cases

--- scripts/analyse_single_model.py
+++ scripts/analyse_single_model.py
@@ -300,8 +300,8 @@ with open( pfile ) as f:
 
 pdbedit = pdb_edit.PDBEdit()
 for pdbCode in sorted( mrbumpResults.keys() ):
-    if pdbCode != "1M5I":
-        continue
+    #if pdbCode != "1M5I":
+    #    continue
     
     workdir = os.path.join( rundir, pdbCode )
     if not os.path.isdir( workdir ):
