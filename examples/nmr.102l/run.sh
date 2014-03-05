60b5280fcfa790e21db616bc597fdc67a13eb1a1
Jens Thomas <linucks42@gmail.com>
2014-03-05 12:06:53 +0000

    Removed settings that are the default

--- examples/nmr.102l/run.sh
+++ examples/nmr.102l/run.sh
@@ -2,14 +2,13 @@
 
 # NMR ensembling example - 2LC9 is an ensemble model of a minor and transiently formed state of a T4 lysozyme mutant. The target 102l is X-ray data
 
-ccp4-python -u ${CCP4}/bin/ample -mtz 102l.mtz -fasta 102L.fasta -NMR_model_in 2LC9.pdb \
+ccp4-python -u ${CCP4}/bin/ample \
+      -mtz 102l.mtz \
+      -F F -SIGF SIGF -FREE FreeR_flag \
+      -fasta 102L.fasta \
       -name 102l \
+      -NMR_model_in 2LC9.pdb \
       -NMR_Truncate_only True \
-      -use_homs True \
-      -early_terminate True  \
       -use_arpwarp False \
       -molrep_only True \
-      -nproc 1 \
-      -F F -SIGF SIGF -FREE FreeR_flag \
-      -use_arpwarp False \
-      -run_dir ${PWD}
+      -nproc 1
