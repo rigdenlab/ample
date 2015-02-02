45cc8fe06ddf7aa3da0933cae22fd3ae9393e5dd
Jens Thomas <linucks42@gmail.com>
2015-02-02 17:50:35 +0000

    Add unittests

--- python/shelxe.py
+++ python/shelxe.py
@@ -64,14 +64,38 @@ END""".format(F,SIGF,FREE)
 
 class Test(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up paths. Need to do this with setUpClass, as otherwise the __file__
+        variable is updated whenever the cwd is changed in a test and the next test
+        gets the wrong paths.
+        """
+        cls.thisd =  os.path.abspath( os.path.dirname( __file__ ) )
+        paths = cls.thisd.split( os.sep )
+        cls.ample_dir = os.sep.join( paths[ : -1 ] )
+        cls.tests_dir=os.path.join(cls.ample_dir,"tests")
+        cls.testfiles_dir = os.path.join(cls.tests_dir,'testfiles')
+        
+        cls.shelxe_exe=ample_util.find_exe('shelxe')
+
+        return
 
     def test1BYZ(self):
-        
-        pass
+        pdb=os.path.join(self.testfiles_dir,"1BYZ.pdb")
+        mtz=os.path.join(self.testfiles_dir,"1BYZ-cad.mtz")
+        mrPdb=os.path.join(self.testfiles_dir,"1BYZ_phaser_loc0_ALL_poly_ala_trunc_0.486615_rad_1_UNMOD.1.pdb")
+        origin=shelxeOrigin(self.shelxe_exe,pdb,mtz,mrPdb)
+        self.assertEqual(origin,[0.326, 0.19, 0.275])
+        return
     
     def test1D7M(self):
-        
-        pass
+        pdb=os.path.join(self.testfiles_dir,"1D7M.pdb")
+        mtz=os.path.join(self.testfiles_dir,"1D7M-cad.mtz")
+        mrPdb=os.path.join(self.testfiles_dir,"1D7M_phaser_loc0_ALL_SCWRL_reliable_sidechains_trunc_5.241154_rad_1_UNMOD.1.pdb")
+        origin=shelxeOrigin(self.shelxe_exe,pdb,mtz,mrPdb)
+        self.assertEqual(origin,[-0.0, -0.0, 0.5])
+        return
 
 
 if __name__ == "__main__":
