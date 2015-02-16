7d776d045ff15c54315032eade0ca5f25ab478e5
Jens Thomas <linucks42@gmail.com>
2015-02-16 17:54:12 +0000

    Work in progress

--- scripts/resubmit.py
+++ scripts/resubmit.py
@@ -4,34 +4,9 @@ import sys
 
 ample_root="/home/jmht42/ample-dev1"
 sys.path.insert(0,os.path.join(ample_root,"python"))
+
+import ample_util
 import mrbump_results
-import clusterize
-
-#with open("")
-pdb_codes=["1UCS"]
-root="/volatile/jmht42/testset/percent"
-
-for pdb in pdb_codes:
-    mrbd=os.path.join(root,pdb,"ROSETTA_MR_0","MRBUMP")
-    os.chdir(mrbd)
-    
-    res_sum = mrbump_results.ResultsSummary()
-    res_sum.extractResults(mrbd)
-    ensembles=[]
-    for r in res_sum.results:
-        search_dir=r['Search_directory']
-        ensemble=r['ensemble_name']
-        x=os.path.join(search_dir,"results","finished.txt")
-        if r['Solution_Type']=='unfinished':
-           ensembles.append(ensemble)
-           #shutil.rmtree(search_dir)
-           #os.unlink(ensemble+".log")
-    
-    scripts=[ os.path.abspath(e+".sh") for e in ensembles]
-    prep_array(scripts,mrbd)
-    
-    # Submit job
-
 
 def prep_array(jobScripts,jobDir):
     os.chdir(jobDir)
@@ -88,3 +63,31 @@ $script
     return arrayScript
             
             
+#with open("")
+pdb_codes=["1UCS"]
+root="/volatile/jmht42/testset/percent"
+
+for pdb in pdb_codes:
+    mrbd=os.path.join(root,pdb,"ROSETTA_MR_0","MRBUMP")
+    os.chdir(mrbd)
+    
+    res_sum = mrbump_results.ResultsSummary()
+    res_sum.extractResults(mrbd)
+    ensembles=[]
+    for r in res_sum.results:
+        search_dir=r['Search_directory']
+        ensemble=r['ensemble_name']
+        x=os.path.join(search_dir,"results","finished.txt")
+        #if r['Solution_Type']=='unfinished':
+        if r['Solution_Type']=='no_job_directory':
+           ensembles.append(ensemble)
+           if os.path.isfile(search_dir): shutil.rmtree(search_dir)
+           log=ensemble+".log"
+           if os.path.isfile(log): os.unlink(log)
+    
+    scripts=[os.path.abspath(e+".sh") for e in ensembles]
+    jscript=prep_array(scripts,mrbd)
+    
+    # Submit job
+    ample_util.run_command(["qsub",jscript])
+
