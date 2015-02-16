e09df2c872394547304d8436897c4399ca63edc7
Jens Thomas <linucks42@gmail.com>
2015-02-16 17:20:01 +0000

    work in progress

--- scripts/resubmit.py
+++ scripts/resubmit.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import sys
 
-ample_root="/opt/ample-dev1"
+ample_root="/home/jmht42/ample-dev1"
 sys.path.insert(0,os.path.join(ample_root,"python"))
 import mrbump_results
 
@@ -14,5 +14,12 @@ for pdb in pdb_codes:
     mrbd=os.path.join(root,pdb,"ROSETTA_MR_0","MRBUMP")
     os.chdir(mrbd)
     
-    for res in ResultsSummary().extractResults(mrbd).results:
-        print "GOT ",r['JobDirectory'],r['ensemble_name']
+    res_sum = mrbump_results.ResultsSummary()
+    res_sum.extractResults(mrbd)
+    ensembles=[]
+    for r in res_sum.results:
+        search_dir=r['Search_directory']
+        ensemble=r['ensemble_name']
+        x=os.path.join(search_dir,"results","finished.txt")
+        if r['Solution_Type']=='unfinished':
+           ensembles.append(ensemble) 
