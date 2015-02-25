b5c1cf0cbcbccb714e4a5d12741f37f8965e9b13
Jens Thomas <linucks42@gmail.com>
2015-02-25 17:27:55 +0000

    updated across all directories

--- scripts/final_analysis.py
+++ scripts/final_analysis.py
@@ -1,5 +1,4 @@
 import cPickle
-import glob
 import os
 import shutil
 import sys
@@ -15,7 +14,7 @@ import mrbump_results
 
 root="/volatile/jmht42/testset/percent"
 #pdb_codes=["1MIX", "1P9G", "1UCS", "1XKR", "2BL2", "2EFR", "2FM9", "2JKU", "2QIH", "2QSK", "2UUI", "2XFD", "2YKT", "3CI9", "3CVF", "3GD8", "3GHF", "3HAP", "3HFE"]
-pdb_codes=["1XKR"]
+pdb_codes=["1MIX", "1P9G", "1UCS",  "2BL2", "2EFR", "2FM9", "2JKU", "2QIH", "2QSK", "2UUI", "2XFD", "2YKT", "3CI9", "3CVF", "3GD8", "3GHF", "3HAP", "3HFE"]
 for pdb in pdb_codes:
     print "PDB ",pdb
     ample_dir=os.path.join(root,pdb,"ROSETTA_MR_0")
@@ -25,6 +24,8 @@ for pdb in pdb_codes:
     res_sum = mrbump_results.ResultsSummary()
     res_sum.extractResults(mrbumpd)
     amoptd['mrbump_results'] = res_sum.results
+    shutil.rmtree(amoptd['benchmark_dir'])
+    os.mkdir(amoptd['benchmark_dir'])
     benchmark.analyse(amoptd)
     ample_util.saveAmoptd(amoptd)
 
