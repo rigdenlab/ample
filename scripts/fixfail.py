141cc8b8e50bc81e2b0c21a861a6236984b3b75b
Jens Thomas <linucks42@gmail.com>
2015-02-28 12:30:07 +0000

    Updated to get more data

--- scripts/fixfail.py
+++ scripts/fixfail.py
@@ -15,7 +15,6 @@ import parse_phaser
 import parse_refmac
             
 pdb_codes=["1MIX", "1P9G", "1UCS", "1XKR", "2BL2", "2EFR", "2FM9", "2JKU", "2QIH", "2QSK", "2UUI", "2XFD", "2YKT", "3CI9", "3CVF", "3GD8", "3GHF", "3HAP", "3HFE"]
-#pdb_codes=["1UCS"]
 root="/volatile/jmht42/testset/percent"
 
 for pdb in pdb_codes:
@@ -26,7 +25,7 @@ for pdb in pdb_codes:
     res_sum = mrbump_results.ResultsSummary()
     res_sum.extractResults(mrbd)
     for r in res_sum.results:
-        if r['Solution_Type']=='PHASER_FAIL' or r['Solution_Type']=='REFMAC_FAIL':
+        if True or r['Solution_Type']=='PHASER_FAIL' or r['Solution_Type']=='REFMAC_FAIL':
             search_dir=r['Search_directory']
             pkl=os.path.join(mrbd,search_dir,'results','resultsTable.pkl')
             with open(pkl) as f: dr=cPickle.load(f)
@@ -34,17 +33,22 @@ for pdb in pdb_codes:
             pd=dr[name]['PHASER']
             #for k in sorted(pd.keys()): print k,pd[k]
             #jd=pd['JobDirectory']
+            print "GOT OLD SOLUTION ",pd['Solution_Type'],pd["PHASER_LLG"]
             pl=pd['PHASER_logfile']
-            pp=parse_phaser.PhaserLogParser(pl)
-            pd["PHASER_version"] = pp.version
-            pd["PHASER_LLG"] = pp.LLG
-            pd["PHASER_TFZ"] = pp.TFZ
-            pd["PHASER_RFZ"] = pp.RFZ
-            pd["PHASER_time"] = pp.time
-            pd["PHASER_killed"] = pp.killed
-            pd["PHASER_error"] = pp.errorStr
+            if os.path.isfile(pl):
+                pp=parse_phaser.PhaserLogParser(pl)
+                pd["PHASER_version"] = pp.version
+                pd["PHASER_time"] = pp.time
+                pd["PHASER_killed"] = pp.killed
+                pd["PHASER_error"] = pp.errorStr
+
+            pdb=pd['PHASER_pdbout']
+            if os.path.isfile(pdb):
+                ppdb=parse_phaser.PhaserPdbParser(pdb)
+                pd["PHASER_LLG"] = ppdb.LLG
+                pd["PHASER_TFZ"] = ppdb.TFZ
+                pd["PHASER_RFZ"] = ppdb.RFZ
             #print "GOT TFZ for {0}: {1}".format(name,pd["PHASER_TFZ"])
-            print "GOT OLD SOLUTION ",pd['Solution_Type']
             if pd['PHASER_error']:
                 print "GOT ERROR for {0}: {1}".format(name,pd["PHASER_error"])
                 pd['Solution_Type']='PHASER_ERROR'
@@ -62,8 +66,8 @@ for pdb in pdb_codes:
                     pd['Solution_Type']='MARGINAL'
                 else:
                     pd['Solution_Type']='POOR'
-            print "GOT NEW SOLUTION ",pd['Solution_Type'],pd['final_Rfree']
-            #with open(pkl,'w') as f: cPickle.dump(dr,f)
+            print "GOT NEW SOLUTION ",pd['Solution_Type'],pd['final_Rfree'],pd["PHASER_LLG"]
+            with open(pkl,'w') as f: cPickle.dump(dr,f)
             
             
 
