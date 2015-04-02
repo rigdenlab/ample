3220f0c495a89c07b74c8740cf9aab2defad212a
Jens Thomas <linucks42@gmail.com>
2015-04-02 17:27:20 +0100

    UPdated for new changes

--- examples/nmr.truncate/run.sh
+++ examples/nmr.truncate/run.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
 # NMR ensembling example - 2LC9 is an ensemble model of a minor and transiently formed state of a T4 lysozyme mutant. The target 102l is X-ray data
-
-
-
-AMPLE=${CCP4}/bin/ample
 AMPLE=/opt/ample-dev1/bin/ample.py
+AMPLE=${CCP4}/bin/ample
 
 ccp4-python -u $AMPLE \
 -mtz 102l.mtz \
 -fasta 102L.fasta \
 -name 102l \
 -NMR_model_in 2LC9.pdb \
--NMR_Truncate_only True \
 -quick_mode True
+
