c4f431a097ee6badda25b39f162540554f5487df
Jens Thomas <linucks42@gmail.com>
2015-04-27 13:37:21 +0100

    Work in progress

--- examples/nmr.remodel/run.sh
+++ examples/nmr.remodel/run.sh
@@ -4,7 +4,8 @@
 #
 # Change the below if the path to these external dependencies of AMPLE
 # cannot be found in your standard PATH
-ROSETTA_DIR=/opt/rosetta-3.5
+rosetta_dir=/opt/rosetta-3.5
+rosetta_dir=/data2/opt/rosetta_bin_linux_2015.05.57576_bundle
 
 export PATH=\
 /opt/maxcluster:\
@@ -14,18 +15,20 @@ $PATH
 # This is only required if you are running ample from outside
 # the standard CCP4 directory
 AMPLEDIR=$CCP4/share/ample
-AMPLEDIR=/opt/ample-dev1
+AMPLEDIR=/home/jmht/ample-dev1
 
 ${AMPLEDIR}/bin/ample.py \
       -name 1t00 \
       -mtz 1t00.mtz \
       -fasta 1T00.fasta  \
-      -rosetta_dir $ROSETTA_DIR \
-      -nmr_model_in 1OKD.pdb \
+      -rosetta_dir $rosetta_dir \
+      -nmr_model_in 2DIZ.pdb \
       -nmr_remodel True \
-      -nmr_process 2 \
       -frags_3mers 1t00.200.3mers \
       -frags_9mers 1t00.200.9mers \
-      -quick_mode True \
       -nproc 8 \
+      -submit_cluster True \
+      -submit_qtype sge \
 
+#      -quick_mode True \
+#      -nmr_process 2 \
