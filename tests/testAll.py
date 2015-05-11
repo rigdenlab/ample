921780114f4e265f01a01ac18d16a58bd88e3da7
Jens Thomas <linucks42@gmail.com>
2015-05-11 18:36:30 +0100

    New way of running tests

--- tests/testAll.py
+++ tests/testAll.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env ccp4-python
 """Script to run all the AMPLE tests"""
+import glob
 import os
 import sys
 import unittest
@@ -16,7 +17,15 @@ python_dir = os.path.join(ample_dir,'python')
 sys.path.insert(0,python_dir)
 
 loader = unittest.defaultTestLoader
+# Don't use discover as we don't want to run all tests
+#tests = loader.discover(python_dir, pattern="*.py")
+skip = ['benchmark', 'clusterize', 'mrbump_results', 'rosetta_model']
+test_suite = unittest.TestSuite()
+# Get list of modules that are not in skip and add the test sets to the test suite
+for m in glob.glob(os.path.join(python_dir,"*.py")):
+    m = os.path.splitext(os.path.basename(m))[0]
+    if m not in skip:
+        m = __import__(m)
+        test_suite.addTests(loader.loadTestsFromModule(m))
 
-tests = loader.discover(python_dir, pattern="*.py")
-
-unittest.TextTestRunner(verbosity=2).run(tests) 
+unittest.TextTestRunner(verbosity=2).run(test_suite) 
