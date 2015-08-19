80ffee94ab3dc6cc8083dd72f9dd738dd229e238
Jens Thomas <linucks42@gmail.com>
2015-08-19 14:52:40 +0100

    Fix run script

--- examples/homologs/run.sh
+++ examples/homologs/run.sh
@@ -9,15 +9,15 @@ export PATH=\
 /opt/shelx:\
 $PATH
 
-#$CCP4/bin/ample.py \
-/opt/ample-dev1/bin/ample.py \
+$CCP4/bin/ample.py \
 -fasta 3DCY.fasta \
 -mtz 3dcy-sf.mtz \
 -homologs True \
--mustang_exe /opt/MUSTANG_v3.2.2/bin/mustang-3.2.1 \
--models . \
+-gesamt_exe /opt/ccp4-devtools/install/bin/gesamt \
+-models .  \
 -nproc 8 \
 
 # Add below for running in benchmark mode
+#-mustang_exe /opt/MUSTANG_v3.2.2/bin/mustang-3.2.1 \
 #-alignment_file testthree.afasta \
 #-native_pdb  1DCY.pdb \
