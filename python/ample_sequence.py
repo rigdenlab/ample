49c97f7bc718a0ef2e4b1dba691a096081052210
Jens Thomas <linucks42@gmail.com>
2015-11-17 18:56:19 +0000

    Add resseqs from a pdb to an existing sequence object

--- python/ample_sequence.py
+++ python/ample_sequence.py
@@ -24,6 +24,46 @@ class Sequence(object):
             self.from_pdb(pdbin=pdb)
         return
     
+    def add_pdb_data(self, pdbin):
+        """Add the resseq information from a pdb to ourselves when we already have the sequence information from a fasta file - such as from an alignment
+        We assume that there will be gaps (-) in the sequence and the letters may be upper or lower case
+        Currently this only supports adding data for single-chain pdbs
+        """
+        assert len(self.headers) and len(self.sequences)
+        # Assume the name of the pdb is the first part of the header
+        fname = os.path.basename(pdbin)
+        name, ext = os.path.splitext(fname)
+        assert ext == '.pdb', "PDB files must have extension .pdb"
+        got=False
+        for idx, h in enumerate(self.headers):
+            n = h[1:].split('.')[0]
+            if n == name:
+                got=True
+                break
+        
+        if not got: raise RuntimeError,"Could not find matching pdb name for {0} in headers {1}".format(fname,self.headers)
+        seqd = pdb_edit.sequence_data(pdbin)
+        assert len(seqd) == 1,'Currently only support adding data for single chain pdbs'
+        
+        # Clear out any existing resseqs
+        self.resseqs[idx] = []
+        
+        k1 = seqd.keys()[0]
+        sequence = seqd[k1][0]
+        resseqs = seqd[k1][1]
+        # Loop through both sequences
+        needle = 0
+        GAP='-'
+        for res1 in self.sequences[idx]:
+            if res1 == GAP:
+                self.resseqs[idx].append(None)
+            else:
+                assert res1.upper() == sequence[needle]
+                self.resseqs[idx].append( resseqs[needle] )
+                needle += 1
+        
+        return True
+    
     def from_fasta(self, fasta_file, canonicalise=True, resseq=True):
         name=os.path.splitext(os.path.basename(fasta_file))[0]
         self.name=name
@@ -216,7 +256,52 @@ class Test(unittest.TestCase):
         s1.write_fasta(out_fasta)
         os.unlink(out_fasta)
         return
-              
+
+    def testAddPdbData(self):
+        fasta1 = os.path.join(self.testfiles_dir,'1ujb_2a6pA_3c7tA.afasta')
+        pdbin1 = os.path.join(self.ample_dir, 'examples', 'homologs','1ujb.pdb')
+        pdbin2 = os.path.join(self.ample_dir, 'examples', 'homologs','2a6pA.pdb')
+        pdbin3 = os.path.join(self.ample_dir, 'examples', 'homologs','3c7tA.pdb')
+        s1 = Sequence(fasta=fasta1)
+        s1.add_pdb_data(pdbin1)
+        s1.add_pdb_data(pdbin2)
+        s1.add_pdb_data(pdbin3)
+        
+        
+        p1r = [None, None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 
+               None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 
+               35, 36, 37, 38, 39, 40, 41, 42, 43, 44, None, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, None, None, 70, 71, 72, 73, 
+               74, 75, 76, 77, 78, 79, 80, 81, 82, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 
+               None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 83, None, None, 84, 85, 86, 87, 88, 89, 90, 91, None, 
+               92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, None, None, None, None, None, None, 
+               None, None, 121, 122, 123, None, None, None, 124, 125, None, None, 126, None, None, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, None, None, 139, 140, 141, 142, 
+               143, 144, 145, 146, 147, 148, 149, None, None, 150, 151, None, 152, 153, 154, 155, 156, None, None, None, None, None, None, None, None, None, None]
+        
+        p2r = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 
+               None, None, None, None, None, None, None, None, None, None, None, 23, 24, 25, 26, 27, 28, None, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 
+               50, None, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, None, None, None, None, 74, None, None, 75, None, 76, 77, 78, 79, 80, 81, 82, 
+               83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, None, None, None, None, None, None, None, None, 105, 106, 107, 108, 109, 110, 111, 112, 
+               113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, None, 133, 134, 135, 136, 137, None, None, 138, 139, 140, 141, 142, 143, 144, 145,
+                146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, None, None, None, None, None, None, None, None, 160, 161, 162, 163, None, 164, 165, 166, 167, 168, 169, None, 
+                None, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, None, None, None, 185, 186, 187, 188, 189, 190, 191, 192, None, 193, 194, 195, 196, None, None, 
+                None, None, None, None, None, None, None, None, None, None, None, None]
+ 
+        
+        p3r = [None, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 
+               112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, None, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 
+               146, 147, 148, 149, None, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 
+               180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 
+               215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 
+               250, None, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 
+               284, 285, 286, 287, 288, None, None, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, None, None, 304, None, None, None, 305, 306, 307, 308, 309, 310, 
+               311, 312, 313, 314, 315, None, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330]
+        
+        self.assertEqual(s1.resseqs[0], p1r)
+        self.assertEqual(s1.resseqs[1], p2r)
+        self.assertEqual(s1.resseqs[2], p3r)
+        
+        return
+
     def testFailChar(self):
         
         infasta=""">3HAP:A|PDBID|CHAIN|SEQUENCE
