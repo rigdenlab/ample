40bdb07155476aa345e28eaa5754ccbd2a56b3f5
Felix Simkovic <felixsimkovic@me.com>
2015-11-23 21:32:15 +0000

    Contact options added to ample_options

--- bin/ample.py
+++ bin/ample.py
@@ -13,9 +13,9 @@ import platform
 import sys
 
 # Add the ample python folder to the PYTHONPATH
-#sys.path.append(os.path.join(os.environ["CCP4"], "share", "ample", "python"))
-root = os.sep.join( os.path.abspath(__file__).split( os.sep )[:-2] )
-sys.path.append( os.path.join( root, "python" ) )
+sys.path.append(os.path.join(os.environ["CCP4"], "share", "ample", "python"))
+#root = os.sep.join( os.path.abspath(__file__).split( os.sep )[:-2] )
+#sys.path.append( os.path.join( root, "python" ) )
 
 # python imports
 import argparse
