437021458ffb445412cca4d2be9aca7663f25da3
Jens Thomas <linucks42@gmail.com>
2016-01-02 17:30:12 +0000

    Ready to run on clusters

--- examples/toxd-example/run_tests.py
+++ examples/toxd-example/run_tests.py
@@ -7,39 +7,16 @@ Created on 29 Dec 2015
 import cPickle
 import glob
 import os, sys
+import shutil
 import unittest
 
-"""
-Need:
-name
-arguments
-test_function
-
-To run:
-nproc or submit_queue
-
-each test needs to return the path to the resultsd.pkl
-
-loop through directories
-build up list of jobs and tests
-write scripts, run and wait to complete
-run tests
-report results
-
-scripts_and_tests = []
-for d in dirs:
-   scripts_and_tests.append(d)
-
-
-for  
-    
-
-"""
-
+# Our imports
 sys.path.append('/opt/ample-dev1/python')
 import workers
 
 class AmpleException(Exception): pass
+
+TESTD = {}
 
 def write_script(name, args):
     ample = os.path.join(os.environ['CCP4'],'bin', 'ample.py')
@@ -63,6 +40,50 @@ def write_script(name, args):
     os.chmod(script, 0o777)
     return os.path.abspath(script)
 
+def run(nproc=1,
+        submit_cluster=False,
+        dry_run=False,
+        clean_up=True):
+    # Create scripts and path to resultsd
+    scripts = []
+    for name in TESTD.keys():
+        script = write_script(name, TESTD[name]['args'] + ['-work_dir', name])
+        scripts.append(script)
+        TESTD[name]['resultsd'] = os.path.join(name,'resultsd.pkl')
+        if clean_up:
+            if os.path.isdir(name): shutil.rmtree(name)
+            logfile = name+'.log'
+            if os.path.isdir(logfile): os.unlink(logfile)
+    
+    # Run all the jobs
+    nproc = nproc
+    submit_cluster = submit_cluster
+    submit_qtype = 'SGE'
+    submit_array = True
+    if not dry_run:
+        workers.run_scripts(job_scripts=scripts,
+                            monitor=None,
+                            chdir=True,
+                            nproc=nproc,
+                            job_time=3600,
+                            job_name='test',
+                            submit_cluster=submit_cluster,
+                            submit_qtype=submit_qtype,
+                            submit_queue=None,
+                            submit_array=submit_array,
+                            submit_max_array=None)
+    
+    
+    # Now run the tests
+    for name in TESTD.keys():
+        try:
+            TESTD[name]['test'](TESTD[name]['resultsd'])
+            print "Job \'{0}\' succeeded".format(name)
+        except AmpleException as ae:
+            print "* Job \'{0}\' failed a test: {1}".format(name, ae)
+        except Exception as e:
+            print "*** Job \'{0}\' generated an exeption: {1}".format(name, e)
+
 
 # Go through each test directory
 # 'homologs'
@@ -72,7 +93,6 @@ def write_script(name, args):
 # 'nmr.truncate'
 # 'toxd-example'
 # 'transmembrane.3LBW'
-
 
 # vanilla test
 args_vanilla = [
@@ -84,9 +104,6 @@ args_vanilla = [
     #'-do_mr', 'False',
 ]
 
-
-
-testd = {}
 
 ###############################################################################
 #
@@ -101,18 +118,17 @@ args_rosetta_modelling = args_vanilla + [
     '-nmodels', '30',
 ]
 
-
 def test_rosetta_modelling(resultsd_pkl):
     with open(resultsd_pkl) as f: ad = cPickle.load(f)
     if not ad['models_dir'] or not len(glob.glob(ad['models_dir']+"/*.pdb'")) == 30: raise AmpleException("Incorrect number of models")
     if not ('ensembles' in ad or len(ad['ensembles'])): raise AmpleException("Incorrect number of ensembles")
     if not ('mrbump_results' in ad or len(ad['mrbump_results'])): raise AmpleException("No MRBUMP results")
     if not ad['success']: raise AmpleException("Job did no succeed")
+    if not ad['mrbump_results'][0]['SHELXE_CC'] > 25: raise AmpleException("SHELXE_CC criteria not met")
     return
         
-testd['rosetta_modelling'] = { 'args' : args_rosetta_modelling,
+TESTD['rosetta_modelling'] = { 'args' : args_rosetta_modelling,
                               'test' :  test_rosetta_modelling }
-
 
 ###############################################################################
 #
@@ -128,9 +144,10 @@ def test_from_existing_models(resultsd_pkl):
     if not ad['ensembles'] or not len(ad['ensembles']) == 12: raise AmpleException("Incorrect number of ensembles")
     if not ('mrbump_results' in ad or len(ad['mrbump_results'])): raise AmpleException("No MRBUMP results")
     if not ad['success']: raise AmpleException("Job did no succeed")
+    if not ad['mrbump_results'][0]['SHELXE_CC'] > 25: raise AmpleException("SHELXE_CC criteria not met")
     return
         
-testd['from_existing_models'] = { 'args' : args_from_existing_models,
+TESTD['from_existing_models'] = { 'args' : args_from_existing_models,
                                   'test' :  test_from_existing_models }
 
 ###############################################################################
@@ -150,7 +167,7 @@ def test_from_quark_models(resultsd_pkl):
     if not ad['success']: raise AmpleException("Job did no succeed")
     return
         
-testd['from_quark_models'] = { 'args' : args_from_quark_models,
+TESTD['from_quark_models'] = { 'args' : args_from_quark_models,
                                'test' : test_from_quark_models }
 
 ###############################################################################
@@ -160,70 +177,49 @@ testd['from_quark_models'] = { 'args' : args_from_quark_models,
 ###############################################################################
 
 
-# Create scripts and path to resultsd
-scripts = []
-for name in testd.keys():
-    script = write_script(name, testd[name]['args'] + ['-work_dir', name])
-    scripts.append(script)
-    testd[name]['resultsd'] = os.path.join(name,'resultsd.pkl')
-
-# Run all the jobs
-nproc = 5
-submit_cluster = False
-submit_qtype = 'sge'
-submit_array = True
-if True:
-    workers.run_scripts(job_scripts=scripts,
-                        monitor=None,
-                        chdir=True,
-                        nproc=nproc,
-                        job_time=3600,
-                        job_name='test',
-                        submit_cluster=submit_cluster,
-                        submit_qtype=submit_qtype,
-                        submit_queue=None,
-                        submit_array=submit_array,
-                        submit_max_array=None)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-nproc', type=int, default=1,
+                        help="Number of processors to run on (1 per job)")
+    parser.add_argument('-dry_run', action='store_true', default=False,
+                        help="Don\'t actually run the jobs")
+    parser.add_argument('-submit_cluster', action='store_true', default=False,
+                        help="Submit to a cluster queueing system")
+    
+    args = parser.parse_args()
+    
+    run(nproc=args.nproc,
+        submit_cluster=args.submit_cluster)
 
 
-# Now run the tests
-for name in testd.keys():
-    try:
-        testd[name]['test'](testd[name]['resultsd'])
-        print "Job \'{0}\' succeeded".format(name)
-    except AmpleException as ae:
-        print "* Job \'{0}\' failed a test: {1}".format(name, ae)
-    except Exception as e:
-        print "*** Job \'{0}\' generated an exeption: {1}".format(name, e)
-
-sys.exit()
-
-write_script('making_rosetta_models', args_rosetta)
-write_script('from_existing_models', args_models)
-write_script('from_quark_models', args_models)
-
-
-sys.path.insert(0,'/opt/ample-dev1/bin')
-
-from ample import Ample
-
-AMPLE = Ample()
-#main(args_vanilla)
-AMPLE.run(args_quark)
-
-print "GOT SUCCESS ", AMPLE.amopt.d['success']
-
-#-restart_pkl AMPLE_4/resultsd.pkl \
-#-mrbump_dir AMPLE_0/MRBUMP \
-
-
-# Add below for running from pre-made ensembles
-#-ensembles ./ROSETTA_MR_0/ensembles_1 \
-
-# Add below for running with contact predictions
-#-contact_file toxd_.pconsc2.CASPRR \
-#-contact_file toxd_.gremlin.CASPRR \
-#-bbcontacts_file toxd_.bbcontacts.CASPRR \
-#-constraints_file toxd_.cst \
-#-native_pdb 1DTX_std.pdb \
-#-energy_function FADE_default \
+if False:
+    write_script('making_rosetta_models', args_rosetta)
+    write_script('from_existing_models', args_models)
+    write_script('from_quark_models', args_models)
+    
+    
+    sys.path.insert(0,'/opt/ample-dev1/bin')
+    
+    from ample import Ample
+    
+    AMPLE = Ample()
+    #main(args_vanilla)
+    AMPLE.run(args_quark)
+    
+    print "GOT SUCCESS ", AMPLE.amopt.d['success']
+    
+    #-restart_pkl AMPLE_4/resultsd.pkl \
+    #-mrbump_dir AMPLE_0/MRBUMP \
+    
+    
+    # Add below for running from pre-made ensembles
+    #-ensembles ./ROSETTA_MR_0/ensembles_1 \
+    
+    # Add below for running with contact predictions
+    #-contact_file toxd_.pconsc2.CASPRR \
+    #-contact_file toxd_.gremlin.CASPRR \
+    #-bbcontacts_file toxd_.bbcontacts.CASPRR \
+    #-constraints_file toxd_.cst \
+    #-native_pdb 1DTX_std.pdb \
+    #-energy_function FADE_default \
