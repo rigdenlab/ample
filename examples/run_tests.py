35b82b07534d8aebaace9b42a5fc84628053de25
Jens Thomas <linucks42@gmail.com>
2016-01-03 11:07:27 +0000

    File to run all test cases

---
+++ examples/run_tests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env ccp4-python
+'''
+Created on 29 Dec 2015
+
+@author: jmht
+'''
+import os
+import sys
+
+sys.path.append('/opt/ample-dev1/python')
+import test_funcs
+
+
+dirs = [
+    'homologs',
+    'ideal-helices',
+    'missing-domain.1k04',
+    'nmr.remodel',
+    'nmr.truncate',
+    'toxd-example',
+    'transmembrane.3LBW'
+    ]
+
+dirs = [
+    'toxd-example',
+]
+
+
+argd = test_funcs.parse_args()
+
+owd = os.getcwd()
+for d in dirs:
+    # possibly a bit clunky - we add the test directory to the path so we can import the
+    # test dict and them remove it from the sys.path
+    sys.path.append(d)
+    from test_cases import test_dict
+    os.chdir(d)
+    if argd['clean']:
+        test_funcs.clean(test_dict)
+    else:
+        print "RUNNING TESTS IN DIRECTORY: {0}".format(os.path.abspath(d))
+        test_funcs.run(test_dict,**argd)
+    os.chdir(owd)
+    sys.path.pop()
+    
+
