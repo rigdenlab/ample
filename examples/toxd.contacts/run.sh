22e16cc067a63f92a9116915a7448e813ffec03c
Felix Simkovic <felixsimkovic@me.com>
2016-01-11 17:51:55 +0000

    added secondary structure for nicer contact map

--- examples/toxd.contacts/run.sh
+++ examples/toxd.contacts/run.sh
@@ -5,16 +5,14 @@
 #
 
 # Set path to include where shelxe is located
-#export PATH=\
-#/opt/shelx:\
-#$PATH
+export PATH=\
+/opt/shelx:\
+$PATH
 
 # Path to the rosetta directory
-#rosetta_dir=/opt/rosetta-3.5
-rosetta_dir=/opt/rosetta_src_2015.39.58186_bundle
+rosetta_dir=/opt/rosetta-3.5
 
-#$CCP4/bin/ample.py \
-../../bin/ample.py \
+$CCP4/bin/ample.py \
 -rosetta_dir $rosetta_dir \
 -fasta toxd_.fasta \
 -mtz 1dtx.mtz \
@@ -24,10 +22,8 @@ rosetta_dir=/opt/rosetta_src_2015.39.58186_bundle
 -percent 50 \
 -use_shelxe True \
 -nproc 5 \
+-psipred_ss2 toxd_.psipred_ss2 \
 -contact_file toxd_.pconsc2.CASPRR \
--native_pdb  1DTX_std.pdb \
--no_gui True \
--do_mr False \
 
 # Add below for running in benchmark mode
 #-native_pdb  1DTX_std.pdb \
