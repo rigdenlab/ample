34a8ed5dafcfc8db52ec142aebfd59edd20110de
Felix Simkovic <felixsimkovic@me.com>
2016-01-11 07:48:52 +0000

    statistics functions in one place

---
+++ python/ample_statistics.py
@@ -0,0 +1,14 @@
+
+
+import numpy
+
+    
+def mean(self, data): 
+    return numpy.mean(data)
+    
+def median(self, data): 
+    return numpy.median(data)
+
+def variance(self, data): 
+    return numpy.var(data)
+
