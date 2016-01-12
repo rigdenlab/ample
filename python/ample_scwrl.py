996a4edd562247f3a7256710afc7270a4e1e0782
Jens Thomas <linucks42@gmail.com>
2016-01-12 15:39:23 +0000

    Remove OXT atoms from the output after we've run SCWRL - these are occasionally added and cause problems as Theseus requires the files have the same number of atoms

--- python/ample_scwrl.py
+++ python/ample_scwrl.py
@@ -7,6 +7,7 @@ import os
 import logging
 
 import ample_util
+import pdb_edit
 
 class Scwrl( object ):
     
@@ -17,9 +18,14 @@ class Scwrl( object ):
         self.scwrl_exe = scwrl_exe
         return
     
-    def add_sidechains(self, pdbin=None, pdbout=None, sequence=None, hydrogens=False):
+    def add_sidechains(self, pdbin=None, pdbout=None, sequence=None, hydrogens=False, strip_oxt=True):
         """Add the specified sidechains to the pdb"""
-        cmd = [ self.scwrl_exe, "-i", pdbin, "-o", pdbout ]
+        
+        _pdbout = pdbout
+        if strip_oxt:
+            _pdbout = pdbout+"_OXT"
+        
+        cmd = [ self.scwrl_exe, "-i", pdbin, "-o", _pdbout ]
         
         # Not needed by default
         if sequence is not None:
@@ -38,20 +44,25 @@ class Scwrl( object ):
             raise RuntimeError,"Error running Scwrl - please check the logfile: {0}".format(logfile)
         else:
             os.unlink(logfile)
+            
+        if strip_oxt:
+            # Remove all OXT atoms
+            pdb_edit.strip(_pdbout, pdbout, atom_types=['OXT'])
+            os.unlink(_pdbout)
+            
         return os.path.abspath(pdbout)
     
-    def process_directory(self, in_dir, out_dir, prefix="scwrl" ):
+    def process_directory(self, in_dir, out_dir, strip_oxt=True, prefix="scwrl" ):
         logging.info('Adding sidechains with SCWRL to models in directory: {0}'.format(in_dir))
-        self.process_models(glob.glob( os.path.join( in_dir, '*.pdb') ), out_dir, prefix=prefix)
+        self.process_models(glob.glob( os.path.join( in_dir, '*.pdb') ), out_dir, strip_oxt=strip_oxt, prefix=prefix)
         return
     
-    def process_models(self, models, out_dir, prefix="scwrl" ):
+    def process_models(self, models, out_dir, strip_oxt=True, prefix="scwrl"):
         logging.info('Adding sidechains with SCWRL to models')
         out_pdbs = []
         for i, pdb in enumerate(models):
             out_pdbs.append(self.add_sidechains(pdbin=pdb,
-                                                pdbout=ample_util.filename_append(pdb,
-                                                                                  prefix,
-                                                                                  directory=out_dir)))
+                                                pdbout=ample_util.filename_append(pdb, prefix, directory=out_dir),
+                                                strip_oxt=strip_oxt))
         logging.info('Processed {0} models with SCWRL into directory: {1}'.format(i+1, out_dir))
         return out_pdbs
