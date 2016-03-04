f97dfa700e0f79c20e8b5bd85138fa076c1cfa3e
Felix Simkovic <felixsimkovic@me.com>
2016-03-04 07:47:24 +0000

    Adapted for faster reaching of successful case

--- examples/single-model/test_cases.py
+++ examples/single-model/test_cases.py
@@ -27,7 +27,7 @@ args_vanilla = [
 #
 ###############################################################################
 args_from_single_model = args_vanilla + [
-    [ '-percent', '15' ],
+    [ '-percent', '20' ],
     [ '-single_model', '3c7t.pdb' ],
     [ '-truncation_scorefile', '3c7t_scores.csv' ],
     [ '-truncation_scorefile_header', 'residue', 'concoord' ],                                         
@@ -39,7 +39,7 @@ class AMPLETest(test_funcs.AMPLEBaseTest):
     def test_from_single_model(self):
         self.assertTrue(self.AMPLE_DICT['AMPLE_finished'])
         nensembles = len(self.AMPLE_DICT['ensembles'])
-        self.assertEqual(nensembles, 21, "Incorrect number of ensembles produced: {0}".format(nensembles))
+        self.assertEqual(nensembles, 15, "Incorrect number of ensembles produced: {0}".format(nensembles))
         self.assertIn('ensembles', self.AMPLE_DICT)
         self.assertGreater(len(self.AMPLE_DICT['ensembles']), 0, "No ensembles produced")
         self.assertIn('mrbump_results', self.AMPLE_DICT)
