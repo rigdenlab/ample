0dd86c1ae5360a7b57c2df13e8205e1fa855bc33
Felix Simkovic <felixsimkovic@me.com>
2016-03-10 13:40:54 +0000

    ample_main renamed to main

--- __main__.py
+++ __main__.py
@@ -2,11 +2,11 @@
 
 import sys
 
-from ample import ample_main
+from ample import main
 from ample.python import ample_exit
 
 try:
-    ample_main.Ample().run()
+    main.Ample().run()
 except Exception as e:
     msg = "Error running main AMPLE program: {0}".format(e.message)
     ample_exit.exit_error(msg, sys.exc_info()[2])
