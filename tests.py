064820edfbf7a455018c97d78a5387d2f41b9a9b
Felix Simkovic <felixsimkovic@me.com>
2016-03-10 18:02:58 +0000

    Framework to run all unittests at once

---
+++ tests.py
@@ -0,0 +1,8 @@
+
+
+import unittest
+
+if __name__ == "__main__":
+    
+    suite = unittest.TestLoader().discover(".", pattern="test*.py")
+    unittest.TextTestRunner(verbosity=2, buffer=True).run(suite)
