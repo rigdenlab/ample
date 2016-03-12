3e2d7a8cd0cdf9145bc491deb9099803c1699365
Felix Simkovic <felixsimkovic@me.com>
2016-03-12 11:44:16 +0000

    dssp parser now in parser dir

--- parsers/dssp_parser.py
+++ parsers/dssp_parser.py
@@ -1,5 +1,3 @@
-import os
-import unittest
 
 class DsspParser(object):
     """
@@ -26,8 +24,6 @@ class DsspParser(object):
     def parse(self):
         """Info from: http://swift.cmbi.ru.nl/gv/dssp/HTML/descrip.html
         """
-
-        # print os.path.join(os.getcwd(), logfile)
 
         self.chainIds = []
         self.resNames = []
@@ -124,51 +120,6 @@ class DsspParser(object):
             return name
 
 
-class Test(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Set up paths. Need to do this with setUpClass, as otherwise the __file__
-        variable is updated whenever the cwd is changed in a test and the next test
-        gets the wrong paths.
-        """
-        cls.thisd =  os.path.abspath( os.path.dirname( __file__ ) )
-        paths = cls.thisd.split( os.sep )
-        cls.ample_dir = os.sep.join( paths[ : -1 ] )
-        cls.tests_dir=os.path.join(cls.ample_dir,"tests")
-        cls.testfiles_dir = os.path.join(cls.tests_dir,'testfiles')
-        return
-
-    def testParse1(self):
-        """parse 2bhw"""
-        os.chdir(self.thisd) # Need as otherwise tests that happen in other directories change os.cwd()
-        
-        dsspLog = os.path.join(self.testfiles_dir,"2bhw.dssp")
-        dsspP = DsspParser( dsspLog )
-        a = [' ', ' ', 'T', 'T', ' ', 'T', 'T', 'S', 'S', 'T', 'T', ' ', ' ', ' ', 'T', 'T', 'G', 'G', 'G', ' ', ' ', 'S', ' ', ' ', 'T', 'T', ' ', ' ', 'S', ' ', 'S', 'T', 'T', ' ', ' ', 'S', ' ', ' ', 'T', 'T', ' ', 'T', 'T', ' ', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'T', 'T', 'T', ' ', ' ', ' ', 'S', ' ', ' ', 'S', 'G', 'G', 'G', 'S', 'G', 'G', 'G', 'G', 'G', 'S', 'T', 'T', ' ', 'E', 'E', 'G', 'G', 'G', ' ', 'T', 'T', 'S', 'E', 'E', 'E', ' ', ' ', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'E', 'E', 'T', 'T', 'E', 'E', ' ', 'S', 'S', 'S', 'S', ' ', ' ', ' ', 'T', 'T', 'S', ' ', 'T', 'T', ' ', 'T', 'T', ' ', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', ' ', 'S', ' ', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', ' ', 'T', 'T', 'T', 'S', 'S', 'G', 'G', 'G', 'G', 'T', 'T', 'T', 'T', ' ', 'S', ' ', ' ']
-        self.assertEqual( dsspP.assignment[0], a)
-        
-        return
-        
-    def testParse2(self):
-        os.chdir(self.thisd) # Need as otherwise tests that happen in other directories change os.cwd()
-
-        dsspLog = os.path.join(self.testfiles_dir,"3ouf.dssp")
-        dsspP = DsspParser( dsspLog )
-        
-        return
-
-def testSuite():
-    suite = unittest.TestSuite()
-    suite.addTest(Test('testParse1'))
-    suite.addTest(Test('testParse2'))
-    return suite
-    
-#
-# Run unit tests
-if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(testSuite())
 
 
 
---
+++ parsers/tests/test_dssp.py
@@ -0,0 +1,54 @@
+
+import os
+import unittest
+from ample.parsers import dssp_parser
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up paths. Need to do this with setUpClass, as otherwise the __file__
+        variable is updated whenever the cwd is changed in a test and the next test
+        gets the wrong paths.
+        """
+        cls.thisd =  os.path.abspath( os.path.dirname( __file__ ) )
+        paths = cls.thisd.split( os.sep )
+        cls.ample_dir = os.sep.join( paths[ : -2 ] )
+        cls.tests_dir=os.path.join(cls.ample_dir,"tests")
+        cls.testfiles_dir = os.path.join(cls.tests_dir,'testfiles')
+        return
+
+    def test_parse1(self):
+        os.chdir(self.thisd) # Need as otherwise tests that happen in other directories change os.cwd()
+        
+        dsspLog = os.path.join(self.testfiles_dir,"2bhw.dssp")
+        dsspP = dssp_parser.DsspParser( dsspLog )
+        a = [' ', ' ', 'T', 'T', ' ', 'T', 'T', 'S', 'S', 'T', 'T', ' ', ' ', 
+             ' ', 'T', 'T', 'G', 'G', 'G', ' ', ' ', 'S', ' ', ' ', 'T', 'T', 
+             ' ', ' ', 'S', ' ', 'S', 'T', 'T', ' ', ' ', 'S', ' ', ' ', 'T', 
+             'T', ' ', 'T', 'T', ' ', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'T', 
+             'T', 'T', ' ', ' ', ' ', 'S', ' ', ' ', 'S', 'G', 'G', 'G', 'S', 
+             'G', 'G', 'G', 'G', 'G', 'S', 'T', 'T', ' ', 'E', 'E', 'G', 'G', 
+             'G', ' ', 'T', 'T', 'S', 'E', 'E', 'E', ' ', ' ', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'E', 'E', 'T', 'T', 'E', 'E', ' ', 'S', 
+             'S', 'S', 'S', ' ', ' ', ' ', 'T', 'T', 'S', ' ', 'T', 'T', ' ', 
+             'T', 'T', ' ', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 
+             'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', ' ', 'S', ' ', 
+             'S', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', ' ', 'T', 'T', 
+             'T', 'S', 'S', 'G', 'G', 'G', 'G', 'T', 'T', 'T', 'T', ' ', 'S', 
+             ' ', ' ']
+        self.assertEqual( dsspP.assignment[0], a)
+        
+    def test_parse2(self):
+        os.chdir(self.thisd) # Need as otherwise tests that happen in other directories change os.cwd()
+
+        dsspLog = os.path.join(self.testfiles_dir,"3ouf.dssp")
+        dsspP = dssp_parser.DsspParser( dsspLog )
+
+if __name__ == "__main__":
+    unittest.main()
