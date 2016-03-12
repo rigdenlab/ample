088cee91155aa078a21c0b106de622dcb3c165f7
Felix Simkovic <felixsimkovic@me.com>
2016-03-12 12:33:35 +0000

    unittest split

--- python/spicker.py
+++ python/spicker.py
@@ -7,7 +7,6 @@ import os
 import re
 import shutil
 import sys
-import unittest
 
 # our imports
 try:
@@ -253,65 +252,6 @@ class Spickerer(object):
             rstr += "\n"
             
         return rstr
-
-class Test(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Set up paths. Need to do this with setUpClass, as otherwise the __file__
-        variable is updated whenever the cwd is changed in a test and the next test
-        gets the wrong paths.
-        """
-        cls.thisd = os.path.abspath(os.path.dirname(__file__))
-        paths = cls.thisd.split(os.sep)
-        cls.ample_dir = os.sep.join(paths[ :-1 ])
-        cls.tests_dir = os.path.join(cls.ample_dir, "tests")
-        cls.testfiles_dir = os.path.join(cls.tests_dir, 'testfiles')
-        
-        spicker_exe = None
-        if 'CCP4' in os.environ:
-            spicker_exe = os.path.join(os.environ["CCP4"],"bin","spicker")
-        else:
-            spicker_exe = ample_util.find_exe('spicker')
-        cls.spicker_exe = spicker_exe
-
-        root = logging.getLogger()
-        root.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.DEBUG)
-        # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        formatter = logging.Formatter('%(message)s')
-        ch.setFormatter(formatter)
-        root.addHandler(ch)
-        return
-    
-    def testSpicker(self):
-        mdir = os.path.join(self.testfiles_dir, "models")
-        models = glob.glob(mdir + os.sep + "*.pdb")
-        
-        work_dir = os.path.join(self.tests_dir, "spicker")
-        if os.path.isdir(work_dir): shutil.rmtree(work_dir)
-        os.mkdir(work_dir)
-
-        spickerer = Spickerer(spicker_exe=self.spicker_exe)
-        spickerer.cluster(models, run_dir=work_dir)
-
-        # This with spicker from ccp4 6.5.010 on osx 10.9.5
-        names = sorted([os.path.basename(m) for m in spickerer.results[0].pdbs])
-        ref = ['5_S_00000005.pdb', '4_S_00000005.pdb', '5_S_00000004.pdb', '4_S_00000002.pdb',
-                '4_S_00000003.pdb', '3_S_00000006.pdb', '3_S_00000004.pdb', '2_S_00000005.pdb',
-                '2_S_00000001.pdb', '3_S_00000003.pdb', '1_S_00000005.pdb', '1_S_00000002.pdb',
-                '1_S_00000004.pdb']
-        self.assertEqual(names, sorted(ref)) # seem to get different results on osx
-        self.assertEqual(len(names), len(ref)) 
-        
-        # Centroid of third cluster
-        self.assertEqual(os.path.basename(spickerer.results[2].cluster_centroid), '1_S_00000001.pdb')
-        
-        shutil.rmtree(work_dir)
-        
-        return
 
 if __name__ == "__main__":
     import argparse, subprocess, tempfile
---
+++ python/tests/test_spicker.py
@@ -0,0 +1,52 @@
+
+import glob
+import os
+import shutil
+import unittest
+from ample.python import spicker
+from ample.util import ample_util
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up paths. Need to do this with setUpClass, as otherwise the __file__
+        variable is updated whenever the cwd is changed in a test and the next test
+        gets the wrong paths.
+        """
+        cls.thisd = os.path.abspath(os.path.dirname(__file__))
+        paths = cls.thisd.split(os.sep)
+        cls.ample_dir = os.sep.join(paths[ :-2 ])
+        cls.tests_dir = os.path.join(cls.ample_dir, "tests")
+        cls.testfiles_dir = os.path.join(cls.tests_dir, 'testfiles')
+        
+        spicker_exe = None
+        if 'CCP4' in os.environ:
+            spicker_exe = os.path.join(os.environ["CCP4"],"bin","spicker")
+        else:
+            spicker_exe = ample_util.find_exe('spicker')
+        cls.spicker_exe = spicker_exe
+    
+    def testSpicker(self):
+        mdir = os.path.join(self.testfiles_dir, "models")
+        models = glob.glob(mdir + os.sep + "*.pdb")
+        work_dir = os.path.join(self.tests_dir, "spicker")
+        if os.path.isdir(work_dir): shutil.rmtree(work_dir)
+        os.mkdir(work_dir)
+        spickerer = spicker.Spickerer(spicker_exe=self.spicker_exe)
+        spickerer.cluster(models, run_dir=work_dir)
+        # This with spicker from ccp4 6.5.010 on osx 10.9.5
+        names = sorted([os.path.basename(m) for m in spickerer.results[0].pdbs])
+        ref = ['5_S_00000005.pdb', '4_S_00000005.pdb', '5_S_00000004.pdb', '4_S_00000002.pdb',
+                '4_S_00000003.pdb', '3_S_00000006.pdb', '3_S_00000004.pdb', '2_S_00000005.pdb',
+                '2_S_00000001.pdb', '3_S_00000003.pdb', '1_S_00000005.pdb', '1_S_00000002.pdb',
+                '1_S_00000004.pdb']
+        self.assertEqual(names, sorted(ref)) # seem to get different results on osx
+        self.assertEqual(len(names), len(ref)) 
+        # Centroid of third cluster
+        self.assertEqual(os.path.basename(spickerer.results[2].cluster_centroid), '5_S_00000006.pdb')
+        shutil.rmtree(work_dir)
+
+if __name__ == "__main__":
+    unittest.main()
