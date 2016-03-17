26f526cc1277785555723adb7c51c6df15346a10
Felix Simkovic <felixsimkovic@me.com>
2016-03-17 14:51:26 +0000

    finding executable now platform independet

--- util/tests/test_ample_util.py
+++ util/tests/test_ample_util.py
@@ -10,18 +10,14 @@ class Test(unittest.TestCase):
     def test_ccp4_version(self):
         i, j, k = ample_util.ccp4_version()
         self.assertEqual((i, j, 0),(7, 0, 0))
-
-    @unittest.skipIf(sys.platform.startswith("win"), "-> on Windows")
-    def test_find_exe_UNIX(self):
-        self.assertTrue("gesamt", os.path.basename(ample_util.find_exe("gesamt")))
-        self.assertTrue("spicker", os.path.basename(ample_util.find_exe("spicker")))
-        self.assertTrue("theseus", os.path.basename(ample_util.find_exe("theseus")))
-        
-    @unittest.skipUnless(sys.platform.startswith("win"), "-> on Unix")
-    def test_find_exe_WINDOWS(self):
-        self.assertTrue("gesamt.exe", os.path.basename(ample_util.find_exe("gesamt")))
-        self.assertTrue("spicker.exe", os.path.basename(ample_util.find_exe("spicker")))
-        self.assertTrue("theseus.exe", os.path.basename(ample_util.find_exe("theseus")))
+    
+    def test_find_exe(self):
+        gesamt_exe = os.path.basename(ample_util.find_exe("gesamt" + ample_util.EXE_EXT))
+        self.assertTrue("gesamt.exe", gesamt_exe)
+        spicker_exe = os.path.basename(ample_util.find_exe("spicker" + ample_util.EXE_EXT))
+        self.assertTrue("spicker.exe", spicker_exe)
+        theseus_exe = os.path.basename(ample_util.find_exe("theseus" + ample_util.EXE_EXT))
+        self.assertTrue("theseus", theseus_exe)
         
 if __name__ == "__main__":
     unittest.main()
