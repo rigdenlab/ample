798f0b539f624fd9b3e8e259307de36e93596056
Felix Simkovic <felixsimkovic@me.com>
2016-03-18 11:06:26 +0000

    bug on windows invoking with module import

--- testing/__main__.py
+++ testing/__main__.py
@@ -1,8 +1,28 @@
 """main routine for Ample testing"""
+import os
 import sys
 from ample.util import exit_util
 from ample.testing import run_tests
 
+#############################################################################
+## Multiprocessing crashes on Windows when running multiple jobs.
+#  Issue recorded 
+#       1) https://docs.python.org/2/library/multiprocessing.html#windows
+if sys.platform.startswith("win"):
+    msg = """
+*****************************************************************************
+A bug prevents you from invoking our testing framework via the module loader. 
+                                                                              
+Please invoke using the following command:                                    
+                                                                              
+% ccp4-python {0}{1}run_tests.py <command> [<args>]                           
+*****************************************************************************
+"""
+    print msg.format(os.path.dirname(__file__), os.sep)
+    sys.exit(1)
+
+#############################################################################
+## On Unix systems we can run as normal
 try:
     run_tests.main()
 except Exception as e:
