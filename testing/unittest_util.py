15b7c8f00dd98a8ef4dc9fafe5b34370fccd4aeb
Felix Simkovic <felixsimkovic@me.com>
2016-03-23 13:29:46 +0000

    more general framework usable my other programs

--- testing/unittest_util.py
+++ testing/unittest_util.py
@@ -5,6 +5,7 @@
 # Author: hlfsimko
 # Date: 21-Mar-2016
 
+import glob
 import os
 from ample.testing.constants import AMPLE_DIR
 from unittest import TestLoader, TextTestRunner, TestSuite
@@ -12,27 +13,41 @@ from unittest import TestLoader, TextTestRunner, TestSuite
 class AMPLEUnittestFramework(object):
     """Framework to run Ample unittesting"""
     
-    def run(self, buffer=True, cases=None, pattern="test*.py", verbosity=2):
-        """main routine for running the test cases
+    def run(self, buffer=False, cases=None, pattern="test*.py", verbosity=2):
+        """main routine for running the test cases"""
         
-        :buffer: display STDOUT messages
-        :case: list of folders to be tested
-        :pattern: test file name pattern
-        :verbosity: chatty output
-        """
-        if cases:
-            suite = self._load_for_subselection(cases, pattern)
-        else:
-            suite = TestLoader().discover(AMPLE_DIR, pattern=pattern,
-                                          top_level_dir=AMPLE_DIR)
-        TextTestRunner(verbosity=verbosity, buffer=buffer).run(suite)
+        suite = SuiteLoader().load_suite(AMPLE_DIR, cases=cases, pattern=pattern)
+        
+        if int(suite.countTestCases()) > 0:
+            TextTestRunner(verbosity=verbosity, buffer=buffer).run(suite)
+            
+        return
 
-    def _load_for_subselection(self, cases, pattern):
+class SuiteLoader(object):
+    
+    def load_suite(self, directory, pattern="test*.py", cases=None):
+        """function to load a unittest test suite"""
+        # If we do not have any test cases then we can search for some in
+        # the specified directory.
+        if not cases:
+            search_pattern = os.path.join(directory, "*")
+            cases = [ os.path.basename(folder) for folder in \
+                        glob.glob(search_pattern) if os.path.isdir(folder) ]
+            
+        suite = self._load_suite(cases, pattern, directory)
+        return suite
+
+    def _load_suite(self, cases, pattern, directory):
         suite = TestSuite()
         for case in cases:
-            path = os.path.join(AMPLE_DIR, case)
-            _suite = TestLoader().discover(path, pattern=pattern,
-                                           top_level_dir=AMPLE_DIR)
-            suite.addTests(_suite)
-            del _suite
+            path = os.path.join(directory, case)
+            
+            try:
+                _suite = TestLoader().discover(path, pattern=pattern,
+                                               top_level_dir=directory)
+                suite.addTests(_suite)
+                del _suite
+            except ImportError:
+                print "*** no test cases found for: {0} ***".format(case)
+            
         return suite
