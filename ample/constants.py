79519c1a4fbcac7cd9973488ecc075ca49f7b45d
Felix Simkovic <felixsimkovic@me.com>
2016-03-24 18:04:45 +0000

    constant file for all of AMPLE

---
+++ ample/constants.py
@@ -0,0 +1,7 @@
+
+import os
+
+__all__ = ["AMPLE_DIR", "SHARE_DIR"]
+
+AMPLE_DIR = os.path.join(os.environ["CCP4"], "lib", "py2", "ample")
+SHARE_DIR = os.path.join(os.environ["CCP4"], "share", "ample")
