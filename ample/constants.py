871c7c4f8c2b8f813d76e6d1c778703fb21a407b
Felix Simkovic <felixsimkovic@me.com>
2016-03-26 10:08:27 +0000

    check for CCP4 in environment

--- ample/constants.py
+++ ample/constants.py
@@ -1,7 +1,13 @@
 
 import os
 
+if "CCP4" not in os.environ.keys(): 
+    msg = "Cannot find CCP4 root directory"
+    raise RuntimeError(msg)
+
 __all__ = ["AMPLE_DIR", "SHARE_DIR"]
 
+# AMPLE source code directory
 AMPLE_DIR = os.path.join(os.environ["CCP4"], "lib", "py2", "ample")
+# AMPLE share data directory
 SHARE_DIR = os.path.join(os.environ["CCP4"], "share", "ample")
