335d1002732d2e1397fa43d394be9207fadf3b61
Felix Simkovic <felixsimkovic@me.com>
2016-03-28 10:48:43 +0100

    bug fix

--- examples/missing-domain.1k04/test_cases.py
+++ examples/missing-domain.1k04/test_cases.py
@@ -15,36 +15,37 @@ from ample.testing.integration_util import AMPLEBaseTest
 INPUT_DIR = os.path.join(SHARE_DIR, "examples", "missing-domain.1k04", "input")
 TEST_DICT = {}
 
-###############################################################################
-#
-# Missing Domain
-#
-###############################################################################
-
-# Specify the arguments to AMPLE to run this test case
-args_missing_domain =  [
-       [ '-fasta', os.path.join(INPUT_DIR, '1k04_.fasta') ],
-       [ '-mtz', os.path.join(INPUT_DIR, '1k04_cad-unique.mtz') ],
-       [ '-domain_all_chains_pdb', os.path.join(INPUT_DIR, 'Known_40.pdb') ],
-       [ '-missing_domain', 'True' ],
-       [ '-frags_3mers', os.path.join(INPUT_DIR, 'aa1k04_03_05.200_v1_3') ],
-       [ '-frags_9mers', os.path.join(INPUT_DIR, 'aa1k04_09_05.200_v1_3') ],
-       [ '-rosetta_dir', '/opt/rosetta-3.5' ],
-]
-
-class AMPLETest(AMPLEBaseTest):
-    def test_missing_domain(self):
-        self.assertTrue(self.AMPLE_DICT['AMPLE_finished'])
-        self.assertIn('mrbump_results', self.AMPLE_DICT)
-        self.assertGreater(len(self.AMPLE_DICT['mrbump_results']), 0, "No MRBUMP results")
-        self.assertTrue(self.AMPLE_DICT['success'])
-        self.assertGreater(self.AMPLE_DICT['mrbump_results'][0]['SHELXE_CC'], 25,"SHELXE_CC criteria not met")
-        return
-
-# Add everything to the test_dict - the key is used to name the script and run directory
-TEST_DICT['missing_domain'] = { 'args' : args_missing_domain,
-                               'test' :  AMPLETest,
-                                }
+if not sys.platform.startswith('win'):
+    ############################################################################
+    #
+    # Missing Domain
+    #
+    ############################################################################
+    
+    # Specify the arguments to AMPLE to run this test case
+    args_missing_domain =  [
+           [ '-fasta', os.path.join(INPUT_DIR, '1k04_.fasta') ],
+           [ '-mtz', os.path.join(INPUT_DIR, '1k04_cad-unique.mtz') ],
+           [ '-domain_all_chains_pdb', os.path.join(INPUT_DIR, 'Known_40.pdb') ],
+           [ '-missing_domain', 'True' ],
+           [ '-frags_3mers', os.path.join(INPUT_DIR, 'aa1k04_03_05.200_v1_3') ],
+           [ '-frags_9mers', os.path.join(INPUT_DIR, 'aa1k04_09_05.200_v1_3') ],
+           [ '-rosetta_dir', '/opt/rosetta-3.5' ],
+    ]
+    
+    class AMPLETest(AMPLEBaseTest):
+        def test_missing_domain(self):
+            self.assertTrue(self.AMPLE_DICT['AMPLE_finished'])
+            self.assertIn('mrbump_results', self.AMPLE_DICT)
+            self.assertGreater(len(self.AMPLE_DICT['mrbump_results']), 0, "No MRBUMP results")
+            self.assertTrue(self.AMPLE_DICT['success'])
+            self.assertGreater(self.AMPLE_DICT['mrbump_results'][0]['SHELXE_CC'], 25,"SHELXE_CC criteria not met")
+            return
+    
+    # Add everything to the test_dict - the key is used to name the script and run directory
+    TEST_DICT['missing_domain'] = { 'args' : args_missing_domain,
+                                   'test' :  AMPLETest,
+                                    }
 
 ###############################################################################
 #
