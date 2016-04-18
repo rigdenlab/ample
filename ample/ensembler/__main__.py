564945912d765ad757265f4e90c28b4bae1dcb2a
Jens Thomas <linucks42@gmail.com>
2016-04-18 15:45:53 +0100

    Fix creation of work_dir

--- ample/ensembler/__main__.py
+++ ample/ensembler/__main__.py
@@ -57,13 +57,13 @@ else:
     optd = amopt.d 
         
 # Set up the working directory if one doesn't already exist
-if not ('work_dir' in optd and optd['work_dir'] and os.path.isdir(optd['work_dir'])):
+if not ('work_dir' in optd and optd['work_dir']):
     optd['work_dir'] = os.path.join(os.path.abspath(os.path.curdir),ENSEMBLE_DIRNAME)
-    try:
-        os.mkdir(optd['work_dir'])
-    except OSError as e:
-        msg = 'Error making ensemble workdir {0} : {1}'.format(optd['work_dir'],e)
-        exit_util.exit_error(msg, sys.exc_info()[2])
+try:
+    os.mkdir(optd['work_dir'])
+except OSError as e:
+    msg = 'Error making ensemble workdir {0} : {1}'.format(optd['work_dir'],e)
+    exit_util.exit_error(msg, sys.exc_info()[2])
 
 setup_logging()
     
