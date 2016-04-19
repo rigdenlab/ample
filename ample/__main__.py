2355a26c3febd8d69e85dd15a9284f50da2f46ea
Jens Thomas <linucks42@gmail.com>
2016-04-19 11:41:01 +0100

    Protect ample routine to stop it being executed when it is reimported by multiprocessing on windows

--- ample/__main__.py
+++ ample/__main__.py
@@ -1,12 +1,21 @@
-"""main route into Ample"""
+"""Main route into Ample
 
-import sys
+The code here needs to be protected by if __name__ == '__main__'
+to prevent multiprocessing on windows rerunning the main ample code when it
+imports the main module again.
 
-from ample import main
-from ample.util import exit_util
+A bug in multiprocessing means that we can't use the __main__.py file and -m switch
+to ccp4-python under windows.
+"""
 
-try:
-    main.Ample().main()
-except Exception as e:
-    msg = "Error running main AMPLE program: {0}".format(e.message)
-    exit_util.exit_error(msg, sys.exc_info()[2])
+if __name__ == '__main__':
+    import os, sys
+
+    from ample import main
+    from ample.util import exit_util
+
+    try:
+        main.Ample().main()
+    except Exception as e:
+        msg = "Error running main AMPLE program: {0}".format(e.message)
+        exit_util.exit_error(msg, sys.exc_info()[2])
