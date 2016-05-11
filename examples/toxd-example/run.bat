f2bdad177eb8eb07ccfd712273e1a4ce16e08ff9
Jens Thomas <linucks42@gmail.com>
2016-05-11 16:54:47 +0100

    Fixe errors spotted by Adam

--- examples/toxd-example/run.bat
+++ examples/toxd-example/run.bat
@@ -1,18 +1,10 @@
-REM Run ample on windows
-
-REM Need to add path to shelxe
-set PATH=C:\Users\jmht42\Shelx\shelx64;%PATH%
+REM Toxd test case
 
 %CCP4%\bin\ample.bat ^
--fasta input/toxd_.fasta ^
--mtz input/1dtx.mtz ^
+-fasta input\toxd_.fasta ^
+-mtz input\1dtx.mtz ^
 -percent 50 ^
 -quick_mode True ^
 -nproc 6 ^
 -models ..\..\tests\testfiles\models
-
-
-
-
-
 
