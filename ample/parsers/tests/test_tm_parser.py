fe374603d39541b7aad8290f3df844f4835c4617
Felix Simkovic <felixsimkovic@me.com>
2016-07-25 13:36:03 +0100

    renamed tmscore_parser to tm_parser; tests adapted; added parser class for TMalign results in tm_parser

---
+++ ample/parsers/tests/test_tm_parser.py
@@ -0,0 +1,48 @@
+"""Test functions for parsers.tmscore_parser"""
+import os
+import unittest
+from ample import constants
+from ample.parsers import tm_parser
+
+class TestTMscore(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.thisd =  os.path.abspath( os.path.dirname( __file__ ) )
+        cls.ample_share = constants.SHARE_DIR
+        cls.testfiles_dir = os.path.join(cls.ample_share, 'testfiles')
+
+    def test_parse(self):
+        logfile = os.path.join(self.testfiles_dir, "tmscore.log")
+        TM = tm_parser.TMscoreLogParser()
+        TM.parse(logfile)
+
+        self.assertEqual(173, TM.nr_residues_common)
+        self.assertEqual(6.654, TM.rmsd)
+        self.assertEqual(0.5512, TM.tm)
+        self.assertEqual(0.3147, TM.maxsub)
+        self.assertEqual(0.4292, TM.gdtts)
+        self.assertEqual(0.2283, TM.gdtha)
+        
+
+class TestTMalign(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.thisd =  os.path.abspath( os.path.dirname( __file__ ) )
+        cls.ample_share = constants.SHARE_DIR
+        cls.testfiles_dir = os.path.join(cls.ample_share, 'testfiles')
+
+    def test_parse(self):
+        logfile = os.path.join(self.testfiles_dir, "tmalign.log")
+        TM = tm_parser.TMalignLogParser()
+        TM.parse(logfile)
+
+        self.assertEqual(143, TM.nr_residues_common)
+        self.assertEqual(0.70502, TM.tm)
+        self.assertEqual(2.68, TM.rmsd)
+        self.assertEqual(0.182, TM.seq_id)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- ample/parsers/tm_parser.py
+++ ample/parsers/tm_parser.py
@@ -5,9 +5,79 @@ __data__ = "29.06.2016"
 __version__ = "1.0"
 
 
+class TMalignLogParser(object):
+    """
+    Class to mine information from a TMalign log
+
+    Attributes
+    ----------
+    tm : float
+       TemplateModelling score
+    rmsd : float
+       RMSD score
+    nr_residues_common : int
+       Number of residues in common between structures
+    seq_id : float
+       Sequence identity [n_identical/n_aligned]
+
+    Warnings
+    --------
+    TMalign parser cannot distinguish which TM-score to use. Please run TMalign with the ``-a`` flag.
+    """
+
+    def __init__(self):
+        self.tm = 0.0
+        self.rmsd = 0.0
+        self.nr_residues_common = 0
+        self.seq_id = 0.0
+        return
+
+    def parse(self, logfile):
+        """
+        Read a TMalign logfile to extract the scores
+
+        Parameters
+        ----------
+        logfile : str
+           Path to the TMalign logfile
+        """
+        with open(logfile, 'r') as f:
+            for line in iter(f.readline, ''):
+                if line.startswith("Aligned length"):
+                    _line = line.split()
+                    nr_residues_common = int(_line[2][:-1])
+                    rmsd = float(_line[4][:-1])
+                    seq_id = float(_line[-1])
+                if line.startswith("TM-score"):
+                    tm = float(line.split()[1])
+
+        self.set(tm, seq_id, rmsd, nr_residues_common)
+        return
+
+    def set(self, tm, seq_id, rmsd, nr_residues_common):
+        """
+        Manually set the TMalign parser parameters
+        """
+        self.tm = tm
+        self.seq_id = seq_id
+        self.rmsd = rmsd
+        self.nr_residues_common = nr_residues_common
+        return
+
+    def reset(self):
+        """
+        Reset the TMalign parser
+        """
+        self.tm = 0.0
+        self.seq_id = 0.0
+        self.rmsd = 0.0
+        self.nr_residues_common = 0
+        return
+
+
 class TMscoreLogParser(object):
     """
-    Class to mine information from a tmscore log
+    Class to mine information from a TMscore log
 
     Attributes
     ----------
