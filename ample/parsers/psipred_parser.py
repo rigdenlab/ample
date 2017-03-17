325e7b89155361a8831294a485fec6add92f72db
Felix Simkovic <felixsimkovic@me.com>
2017-03-17 12:43:41 +0000

    Pythonized

--- ample/parsers/psipred_parser.py
+++ ample/parsers/psipred_parser.py
@@ -1,15 +1,46 @@
+"""A parser object for Psipred Ss2 files"""
+
+from __future__ import print_function
+
+__author__ = "Felix Simkovic & Jens Thomas"
+__date__ = "13 Jan 2016"
+__version__ = "0.1"
 
 import collections
+import warnings
+
 
 class PsipredSs2Parser(object):
     """Parser for psipred ss2 file"""
     
     def __init__(self, ss2file=None):
         self.residues = None
-        if ss2file: self.parse(ss2file)
+        if ss2file: 
+            self.parse(ss2file)
         
+    @property
+    def secondary_structure(self):
+        """The secondary structure
+
+        Returns
+        -------
+        str
+           The secondary structure one-letter codes
+
+        """
+        if not self.residues:
+            return None
+        return "".join([i.ss for i in self.residues])
+
     def parse(self, ss2file):
-        
+        """Parse a secondary structure file
+
+        Parameters
+        ----------
+        ss2file : str
+           The path to the Psipred ss2 file
+
+        """
         PSIPredResidueInfo = collections.namedtuple("PSIPredResidueInfo", ["rank", "residue", "ss", "coil", "helix", "strand"])
         residues = []
         
@@ -30,25 +61,31 @@ class PsipredSs2Parser(object):
         self.residues = tuple(residues)
         return
         
-    def checkContent(self):
-        # Calculate secondary structure content in sequence          
+    def check_content(self):
+        """Check the secondary structure composition"""
+
         H = len([i for i in self.residues if i.ss=="H"])
         E = len([i for i in self.residues if i.ss=="E"])
 
-        #E_content = float(E) / len(pred) * 100
-        #H_content = float(H) / len(pred) * 100
         if H > 0 and E > 0:
-            print  'Your protein is predicted to be mixed alpha beta, your chances of success are intermediate'
+            print('Your protein is predicted to be mixed alpha beta, your chances of success are intermediate')
         if H == 0 and E > 0:
-            print  'Your protein is predicted to be all beta, your chances of success are low'
+            print('Your protein is predicted to be all beta, your chances of success are low')
         if H > 0 and E == 0:
-            print  'Your protein is predicted to be all alpha, your chances of success are high'
+            print('Your protein is predicted to be all alpha, your chances of success are high')
         if  H == 0 and E == 0:
-            print  'Your protein is has no predicted secondary structure, your chances of success are low'
+            print('Your protein is has no predicted secondary structure, your chances of success are low')
         return
     
+    def checkContent(self):
+        """Check the secondary structure composition"""
+        warnings.warn(DeprecationWarning, "This function will be removed in a future release - use check_content() instead")
+        return self.check_content()
+    
     def getSecondaryStructure(self):
-        return "".join([i.ss for i in self.residues])
+        """Get the secondary structure content"""
+        warnings.warn(DeprecationWarning, "This function will be removed in a future release - use attribute secondary_structure instead")
+        return self.secondary_structure
 
         
 
