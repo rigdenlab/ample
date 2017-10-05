4c422df68c3a03935f8a3ec5afaa1dc3442e7c11
linucks <linucks42@gmail.com>
2017-10-05 17:53:49 +0100

    Take pkl file from command line

--- i2/ample_mock.py
+++ i2/ample_mock.py
@@ -4,6 +4,7 @@ import argparse
 import cPickle
 import logging
 import os
+import pyrvapi
 import shutil
 import sys
 
@@ -16,6 +17,7 @@ logging.basicConfig()
 
 # Get the path to program.xml
 parser = argparse.ArgumentParser()
+parser.add_argument('-ample_pkl')
 parser.add_argument('-ccp4i2_xml')
 parser.add_argument('-own_gui', default=False, action='store_true')
 opt, _ = parser.parse_known_args()
@@ -27,7 +29,7 @@ work_dir = os.path.abspath(I2DIR)
 os.mkdir(work_dir)
 
 # Copy in amopt pkl
-with open(opkl) as f: od = cPickle.load(f)
+with open(opt.ample_pkl) as f: od = cPickle.load(f)
 
 # update paths and copy across old files
 amoptd_fix_path(od, newroot=work_dir, i2mock=True)
@@ -41,3 +43,5 @@ with open(os.path.join(work_dir,AMPLE_PKL), 'w') as w: cPickle.dump(od, w)
 # Run gui and create jsrview files from dict
 AR = AmpleOutput(od, own_gui=opt.own_gui)
 AR.display_results(od)
+
+#pyrvapi.rvapi_store_document2('jens.rvapi')
