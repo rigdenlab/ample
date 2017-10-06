33d8172fa6979ea9be44f8197f8f018accc356a3
jmht <linucks42@gmail.com>
2017-10-06 11:42:44 +0100

    Changes for jscofe

--- i2/ample_mock.py
+++ i2/ample_mock.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env ccp4-python
 
 import argparse
+import copy
 import cPickle
 import logging
 import os
 import pyrvapi
 import shutil
 import sys
+import time
 
 #from ample.util import ample_util
 from ample.util.ample_util import I2DIR, amoptd_fix_path
@@ -22,10 +24,9 @@ parser.add_argument('-ccp4i2_xml')
 parser.add_argument('-own_gui', default=False, action='store_true')
 opt, _ = parser.parse_known_args()
 
-opkl = '/opt/ample.git/ample_testing/from_existing_models/resultsd.pkl'
-
 # Create working directory
 work_dir = os.path.abspath(I2DIR)
+if os.path.isdir(work_dir): shutil.rmtree(work_dir)
 os.mkdir(work_dir)
 
 # Copy in amopt pkl
@@ -42,6 +43,38 @@ with open(os.path.join(work_dir,AMPLE_PKL), 'w') as w: cPickle.dump(od, w)
 
 # Run gui and create jsrview files from dict
 AR = AmpleOutput(od, own_gui=opt.own_gui)
-AR.display_results(od)
+if False:    
+    AR.display_results(od)
+else:
+    SLEEP = 10
+    
+    newd = copy.copy(od)
+    newd['ensembles_data'] = None
+    newd['mrbump_results'] = None
+       
+    AR.display_results(newd)
+    time.sleep(SLEEP)
+       
+    #for i in range(10):
+    newd['ensembles_data'] = od['ensembles_data']
+    AR.display_results(newd)
+    time.sleep(SLEEP)
+    sys.exit()
+       
+    mrbump_results = []
+    for r in ample_dict['mrbump_results'][0:3]:
+        r['SHELXE_CC'] = None
+        r['SHELXE_ACL'] = None
+        mrbump_results.append(r)
+    view1_dict['mrbump_results'] = mrbump_results
+    AR.display_results(view1_dict)
+    time.sleep(SLEEP)
+       
+    view1_dict['mrbump_results'] = ample_dict['mrbump_results'][0:5]
+    AR.display_results(view1_dict)  
+    time.sleep(SLEEP)
+       
+    view1_dict['mrbump_results'] = ample_dict['mrbump_results']
+    AR.display_results(view1_dict) 
 
 #pyrvapi.rvapi_store_document2('jens.rvapi')
