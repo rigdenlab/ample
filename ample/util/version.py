a060f45e16bd6793e00c398c4c62e860fd5b0e8c
Felix Simkovic <felixsimkovic@me.com>
2018-01-29 08:42:19 +0000

    v1.4.1

--- ample/util/version.py
+++ ample/util/version.py
@@ -1,3 +1,3 @@
-__version_info__ = (1, 4, 0)
+__version_info__ = (1, 4, 1)
 __version__ = '.'.join(str(v) for v in __version_info__)
-__git_revno__ = "6a2e589913b048f17a87e54430f0a3ca01f57dcc"
+__git_revno__ = "eb4b9efc362a7d07008edf0201852a41d6dc76e9"
