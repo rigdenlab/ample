4b6e8a377b4c437aa46925d029fc186120a6c35c
Felix Simkovic <felixsimkovic@me.com>
2018-07-10 13:54:39 +0100

    removed files that should not be part of source

--- include/setup_bak.sh
+++
@@ -1,6 +0,0 @@
-export THESEUS_EXE=/share/apps/bin/theseus
-export SCWRL_EXE=/share/apps/bin/Scwrl4
-export PYTHONPATH=$PYTHONPATH:/home/jac45/ample/python
-export ROSETTA_PATH=/home/jac45/rosetta
-export MAX_EXE=/home/jac45/maxcluster
-export SPICKER_EXE=/home/jac45/spicker_dir/spicker
