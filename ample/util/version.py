0227f62a35af514a03ed9b00445b18c498bb31a6
jmht <linucks42@gmail.com>
2018-11-29 17:10:25 +0000

    v1.4.5

--- ample/util/version.py
+++ ample/util/version.py
@@ -1,3 +1,3 @@
-__version_info__ = (1, 4, 4)
+__version_info__ = (1, 4, 5)
 __version__ = '.'.join(str(v) for v in __version_info__)
-__git_revno__ = "59c9923a2f5dc6cc2e93ac5ae2c546dba7d8199a"
+__git_revno__ = "e94355c11add89bb45dd4f369d1a73fa7847878a"
