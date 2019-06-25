a04af7b024b568c3b3ede7992e2b03589d51b97e
jmht <linucks42@gmail.com>
2019-06-25 15:01:09 +0100

    Change git revno

--- ample/util/version.py
+++ ample/util/version.py
@@ -1,3 +1,3 @@
 __version_info__ = (1, 5, 0)
 __version__ = '.'.join(str(v) for v in __version_info__)
-__git_revno__ = "82768c8cdb19318bbb2ca6ccb706ffd864f49365"
+__git_revno__ = "b350b9257de3481d7787ba8993a672245dd29549"
