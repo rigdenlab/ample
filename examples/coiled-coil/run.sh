cc3b1f83bef7493a252a74d9f5770e2375df00bd
hlasimpk <hlasimpk@liv.ac.uk>
2019-10-18 13:04:30 +0100

    Changed coiled-coil test case and added premade models

--- examples/coiled-coil/run.sh
+++ examples/coiled-coil/run.sh
@@ -16,18 +16,27 @@ $CCP4/bin/ample \
 -rosetta_dir $rosetta_dir \
 -fasta input/1g1j.fasta \
 -mtz input/1g1j.mtz \
--frags_3mers input/aa4u5t_03_05.200_v1_3 \
+-frags_3mers input/aat000_03_05.200_v1_3 \
 -frags_9mers input/aat000_09_05.200_v1_3 \
 -multimer_modelling tetramer \
 -nmodels 30 \
 -nmasu 2 \
 -shelxe_rebuild_buccaneer True \
--max_shexle_resolution 4 \
+-shelxe_max_resolution 4 \
 -nproc 5 \
+-models ../../../testfiles/multimer_models \
+
+# Add below for running in benchmark mode
+#-native_pdb  input/1g1j.pdb \
+
+# Add below for running from pre-made ensembles
+#-ensembles ./ROSETTA_MR_0/ensembles_1 \
+
 
 # Additional optional flags
 # Add below to run from pre-made models
-#-models ../../testfiles/models \
+#-make_models False \
+#-models ../../testfiles/multimer_models \
 
 # Add below for running in benchmark mode
 #-native_pdb  input/1g1j.pdb \
