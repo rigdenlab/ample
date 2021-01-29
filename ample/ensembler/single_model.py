7f08649fec2db0ad66f977e3d15d251ea1eac242
hlasimpk <hlasimpk@liverpool.ac.uk>
2021-01-29 17:29:14 +0000

    Final changes to single model mode

--- ample/ensembler/single_model.py
+++ ample/ensembler/single_model.py
@@ -1,6 +1,6 @@
 """Ensembler module for single model structures"""
 
-__author__ = "Felix Simkovic, and Jens Thomas"
+__author__ = "Felix Simkovic, Jens Thomas and Adam Simpkin"
 __date__ = "16 Feb 2016"
 __version__ = "1.0"
 
@@ -9,6 +9,7 @@ import logging
 import math
 import os
 import pandas as pd
+import sys
 
 from ample.ensembler import _ensembler, truncation_util
 from ample.ensembler.constants import SIDE_CHAIN_TREATMENTS
@@ -65,12 +66,12 @@ class SingleModelEnsembler(_ensembler.Ensembler):
 
         std_model = ample_util.filename_append(models[0], 'std', std_models_dir)
         pdb_edit.standardise(pdbin=models[0], pdbout=std_model)
+
         if truncation_method == truncation_util.TRUNCATION_METHODS.ERRORS:
             self._modify_bfactors(std_model, std_model)
 
         std_models = [std_model]
         logger.info('Standardised input model: %s', std_models[0])
-
 
         # Create final ensembles directory
         if not os.path.isdir(self.ensembles_directory):
@@ -90,41 +91,15 @@ class SingleModelEnsembler(_ensembler.Ensembler):
             assert len(truncation_scorefile_header) > 1, "At least two column labels are required"
             residue_scores = self._read_scorefile(truncation_scorefile)
             residue_key = truncation_scorefile_header.pop(0)
-            truncation_scorefile_header = map(str.strip, truncation_scorefile_header)
+            truncation_scorefile_header = list(map(str.strip, truncation_scorefile_header))
             assert all(
                 h in residue_scores[0] for h in truncation_scorefile_header
             ), "Not all column labels are in your CSV file"
             self.ensembles = []
             for score_key in truncation_scorefile_header:
-                zipped_scores = self._generate_residue_scorelist(residue_key, score_key, residue_scores)
-                score_truncate_dir = os.path.join(truncate_dir, "{}".format(score_key))
-                if not os.path.isdir(score_truncate_dir):
-                    os.mkdir(score_truncate_dir)
-
-                self.truncator = truncation_util.Truncator(work_dir=score_truncate_dir)
-                self.truncator.theseus_exe = self.theseus_exe
-                for truncation in self.truncator.truncate_models(
-                    models=std_models,
-                    truncation_method=truncation_method,
-                    percent_truncation=percent_truncation,
-                    percent_fixed_intervals=percent_fixed_intervals,
-                    truncation_pruning=truncation_pruning,
-                    residue_scores=zipped_scores,
-                ):
-
-                    pre_ensemble = _ensembler.Ensemble()
-                    pre_ensemble.num_residues = truncation.num_residues
-                    pre_ensemble.truncation_dir = truncation.directory
-                    pre_ensemble.truncation_level = truncation.level
-                    pre_ensemble.truncation_method = truncation.method
-                    pre_ensemble.truncation_percent = truncation.percent
-                    pre_ensemble.truncation_residues = truncation.residues
-                    pre_ensemble.truncation_variance = truncation.variances
-                    pre_ensemble.truncation_score_key = score_key.lower()
-                    pre_ensemble.pdb = truncation.models[0]
-
-                    for ensemble in self.edit_side_chains(pre_ensemble, side_chain_treatments, single_structure=True):
-                        self.ensembles.append(ensemble)
+                self._generate_ensembles(residue_key, score_key, residue_scores, truncate_dir, std_models,
+                                         truncation_method, percent_truncation, percent_fixed_intervals,
+                                         truncation_pruning, side_chain_treatments)
 
         if truncation_method in [truncation_util.TRUNCATION_METHODS.BFACTORS, truncation_util.TRUNCATION_METHODS.ERRORS]:
             struct = gemmi.read_structure(std_model)
@@ -134,37 +109,43 @@ class SingleModelEnsembler(_ensembler.Ensembler):
             for chain in struct[0]:
                 for residue in chain:
                     residue_scores.append({residue_key: residue.seqid.num, score_key: residue[0].b_iso})
-            zipped_scores = self._generate_residue_scorelist(residue_key, score_key, residue_scores)
-            score_truncate_dir = os.path.join(truncate_dir, "{}".format(score_key))
-            if not os.path.isdir(score_truncate_dir):
-                os.mkdir(score_truncate_dir)
-            self.ensembles = []
-            self.truncator = truncation_util.Truncator(work_dir=score_truncate_dir)
-            self.truncator.theseus_exe = self.theseus_exe
-            for truncation in self.truncator.truncate_models(
-                    models=std_models,
-                    truncation_method=truncation_method,
-                    percent_truncation=percent_truncation,
-                    percent_fixed_intervals=percent_fixed_intervals,
-                    truncation_pruning=truncation_pruning,
-                    residue_scores=zipped_scores,
-            ):
-
-                pre_ensemble = _ensembler.Ensemble()
-                pre_ensemble.num_residues = truncation.num_residues
-                pre_ensemble.truncation_dir = truncation.directory
-                pre_ensemble.truncation_level = truncation.level
-                pre_ensemble.truncation_method = truncation.method
-                pre_ensemble.truncation_percent = truncation.percent
-                pre_ensemble.truncation_residues = truncation.residues
-                pre_ensemble.truncation_variance = truncation.variances
-                pre_ensemble.truncation_score_key = score_key.lower()
-                pre_ensemble.pdb = truncation.models[0]
-
-                for ensemble in self.edit_side_chains(pre_ensemble, side_chain_treatments, single_structure=True):
-                    self.ensembles.append(ensemble)
+            self._generate_ensembles(residue_key, score_key, residue_scores, truncate_dir, std_models,
+                                     truncation_method, percent_truncation, percent_fixed_intervals,
+                                     truncation_pruning, side_chain_treatments)
 
         return self.ensembles
+
+    def _generate_ensembles(self, residue_key, score_key, residue_scores, truncate_dir, std_models, truncation_method,
+                            percent_truncation, percent_fixed_intervals, truncation_pruning, side_chain_treatments):
+        zipped_scores = self._generate_residue_scorelist(residue_key, score_key, residue_scores)
+        score_truncate_dir = os.path.join(truncate_dir, "{}".format(score_key))
+        if not os.path.isdir(score_truncate_dir):
+            os.mkdir(score_truncate_dir)
+        self.ensembles = []
+        self.truncator = truncation_util.Truncator(work_dir=score_truncate_dir)
+        self.truncator.theseus_exe = self.theseus_exe
+        for truncation in self.truncator.truncate_models(
+                models=std_models,
+                truncation_method=truncation_method,
+                percent_truncation=percent_truncation,
+                percent_fixed_intervals=percent_fixed_intervals,
+                truncation_pruning=truncation_pruning,
+                residue_scores=zipped_scores,
+        ):
+
+            pre_ensemble = _ensembler.Ensemble()
+            pre_ensemble.num_residues = truncation.num_residues
+            pre_ensemble.truncation_dir = truncation.directory
+            pre_ensemble.truncation_level = truncation.level
+            pre_ensemble.truncation_method = truncation.method
+            pre_ensemble.truncation_percent = truncation.percent
+            pre_ensemble.truncation_residues = truncation.residues
+            pre_ensemble.truncation_variance = truncation.variances
+            pre_ensemble.truncation_score_key = score_key.lower()
+            pre_ensemble.pdb = truncation.models[0]
+
+            for ensemble in self.edit_side_chains(pre_ensemble, side_chain_treatments, single_structure=True):
+                self.ensembles.append(ensemble)
 
     def generate_ensembles_from_amoptd(self, models, amoptd):
         """Generate ensembles from data in supplied ample data dictionary."""
@@ -177,7 +158,10 @@ class SingleModelEnsembler(_ensembler.Ensembler):
             'truncation_scorefile': amoptd['truncation_scorefile'],
             'truncation_scorefile_header': amoptd['truncation_scorefile_header'],
         }
-        kwargs = {k: v for k, v in kwargs.iteritems() if v is not None}
+        if sys.version_info.major == 3:
+            kwargs = {k: v for k, v in kwargs.items() if v is not None}
+        else:
+            kwargs = {k: v for k, v in kwargs.iteritems() if v is not None}
         return self.generate_ensembles(models, **kwargs)
 
     @staticmethod
@@ -228,4 +212,4 @@ class SingleModelEnsembler(_ensembler.Ensembler):
         """
         df = pd.read_csv(scorefile)
         df.rename(columns=lambda x: x.strip(), inplace=True)
-        return df.T.to_dict().values()
+        return list(df.T.to_dict().values())
